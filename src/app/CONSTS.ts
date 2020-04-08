20de1ea8f83d2e8952fc7d4e7f35e785fea17f88
Ajay Gajra <ajay.gajra@hotmail.com>
2020-04-08 22:16:06 +0530

    v1.0.0-beta3

--- src/app/CONSTS.ts
+++ src/app/CONSTS.ts
@@ -1,6 +1,7 @@
 export const COMETCHAT_CONSTANTS = {
-    APP_ID: '',
-    REGION: '',
-    API_KEY: ''
+    APP_ID: '9992e0c46c915d',
+    REGION: 'US',
+    API_KEY: '606028d3c3ecff1b2014886376054ada6621ef97'
 }
 
+// http://localhost:4200/?uid=ajaygajra&apiKey=606028d3c3ecff1b2014886376054ada6621ef97&appId=9992e0c46c915d&region=us
