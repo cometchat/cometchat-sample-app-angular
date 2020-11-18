97fe08b67837271afa7a1083c6d96e146286ca19
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-11-18 15:32:37 +0530

    Library setup

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/angular-chat-ui-kit.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AngularChatUiKitComponent } from './angular-chat-ui-kit.component';
+
+
+
+@NgModule({
+  declarations: [AngularChatUiKitComponent],
+  imports: [
+  ],
+  exports: [AngularChatUiKitComponent]
+})
+export class AngularChatUiKitModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-chat-ui-kit
+ */
+
+export * from './lib/angular-chat-ui-kit.service';
+export * from './lib/angular-chat-ui-kit.component';
+export * from './lib/angular-chat-ui-kit.module';
