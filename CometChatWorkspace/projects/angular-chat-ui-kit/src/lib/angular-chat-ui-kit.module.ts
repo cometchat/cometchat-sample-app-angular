b5b22067041df66dd576b0b3e35d5aa7ab966de5
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-11-19 19:35:27 +0530

    avatar module added in AngularChatUiKitModule

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/angular-chat-ui-kit.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/angular-chat-ui-kit.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { AngularChatUiKitComponent } from './angular-chat-ui-kit.component';
-import { CometChatUserContactListModule } from './comet-chat-user-contact-list/comet-chat-user-contact-list.module';
-import { CometChatUserContactListComponent } from './comet-chat-user-contact-list/comet-chat-user-contact-list/comet-chat-user-contact-list.component';
-
-
+import { NgModule } from "@angular/core";
+import { AvatarModule } from "angular-chat-ui-kit/lib/avatar/avatar.module";
+import { AngularChatUiKitComponent } from "./angular-chat-ui-kit.component";
+import { CometChatUserContactListModule } from "./comet-chat-user-contact-list/comet-chat-user-contact-list.module";
+import { CometChatUserContactListComponent } from "./comet-chat-user-contact-list/comet-chat-user-contact-list/comet-chat-user-contact-list.component";
 
 @NgModule({
   declarations: [AngularChatUiKitComponent],
-  imports: [
-    CometChatUserContactListModule
+  imports: [CometChatUserContactListModule, AvatarModule],
+  exports: [
+    AngularChatUiKitComponent,
+    CometChatUserContactListModule,
+    AvatarModule,
   ],
-  exports: [AngularChatUiKitComponent  , CometChatUserContactListModule ]
 })
-export class AngularChatUiKitModule { }
+export class AngularChatUiKitModule {}
