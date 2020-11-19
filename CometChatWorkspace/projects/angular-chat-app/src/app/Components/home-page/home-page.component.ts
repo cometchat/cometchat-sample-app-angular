a5a2b73db86aede31edb8c4d0ae2e81bdc98eec3
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-11-19 17:30:32 +0530

    formatting improved

--- CometChatWorkspace/projects/angular-chat-app/src/app/Components/home-page/home-page.component.ts
+++ CometChatWorkspace/projects/angular-chat-app/src/app/Components/home-page/home-page.component.ts
@@ -1,39 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CometChat } from '@cometchat-pro/chat';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { CometChat } from "@cometchat-pro/chat";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.scss"],
 })
 export class HomePageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router: Router) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  /**
+   * Get webApp Info and businessIDs from token data
+   * @param
+   */
+  onLogout() {
+    CometChat.logout().then(
+      (user) => {
+        //console.log("Logout successfull:");
+        this.router.navigate(["/login"]);
+      },
+      (error) => {
+        //console.log("Logout failed", { error });
+      }
+    );
   }
 
-
-   /**
-	 * Get webApp Info and businessIDs from token data
-	 * @param 
-	*/
- onLogout(){
-
-  CometChat.logout().then(
-    user => {
-      //console.log("Logout successfull:");  
-      this.router.navigate(['/login']);
-
-    },
-    error => {
-      //console.log("Logout failed", { error });    
-    }
-  );
-
-  
-   
- }
-
+  /**
+   * Go to contact page
+   *
+   */
+  navigateToContactPage() {
+    this.router.navigate(["/contact-list"]);
+  }
 }
