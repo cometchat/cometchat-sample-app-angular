da175636654498e0dc4daecf2b0904e8ad02e66c
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-23 11:11:08 +0530

    formatted

--- CometChatWorkspace/projects/angular-chat-app/src/app/Components/user-list-page/user-list-page.component.ts
+++ CometChatWorkspace/projects/angular-chat-app/src/app/Components/user-list-page/user-list-page.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-user-list-page',
-  templateUrl: './user-list-page.component.html',
-  styleUrls: ['./user-list-page.component.scss']
+  selector: "app-user-list-page",
+  templateUrl: "./user-list-page.component.html",
+  styleUrls: ["./user-list-page.component.scss"],
 })
 export class UserListPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
