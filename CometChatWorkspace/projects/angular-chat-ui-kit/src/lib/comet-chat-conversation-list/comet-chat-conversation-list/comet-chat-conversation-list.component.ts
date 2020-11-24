44560e555c82ec8d34c12f033248fa1a65076b54
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-24 12:01:30 +0530

    comet-chat-conversation-list added in comet-chat-ui-kit

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-conversation-list/comet-chat-conversation-list/comet-chat-conversation-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-comet-chat-conversation-list',
+  templateUrl: './comet-chat-conversation-list.component.html',
+  styleUrls: ['./comet-chat-conversation-list.component.css']
+})
+export class CometChatConversationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
