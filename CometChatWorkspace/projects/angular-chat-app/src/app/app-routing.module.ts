139dab9267c9d35b2f15c5fceeafe84611d49e11
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-24 14:03:44 +0530

    routing added

--- CometChatWorkspace/projects/angular-chat-app/src/app/app-routing.module.ts
+++ CometChatWorkspace/projects/angular-chat-app/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomePageComponent } from "./Components/home-page/home-page.component";
 import { KitchenSinkAppComponent } from "./Components/kitchen-sink-app/kitchen-sink-app.component";
 import { CometChatConversationListPageComponent } from "./Components/Pages/comet-chat-conversation-list-page/comet-chat-conversation-list-page.component";
+import { CometChatMessageComposerPageComponent } from "./Components/Pages/comet-chat-message-composer-page/comet-chat-message-composer-page.component";
 import { UserListPageComponent } from "./Components/user-list-page/user-list-page.component";
 
 const routes: Routes = [
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: "conversation",
     component: CometChatConversationListPageComponent,
+  },
+  {
+    path: "contact-screen",
+    component: CometChatMessageComposerPageComponent,
   },
 ];
 
