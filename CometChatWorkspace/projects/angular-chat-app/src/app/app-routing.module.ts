8c433692ad768ece3df7ceb4fa342db1bceebd25
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-24 11:56:51 +0530

    routing added for comet-chat-conversation0list

--- CometChatWorkspace/projects/angular-chat-app/src/app/app-routing.module.ts
+++ CometChatWorkspace/projects/angular-chat-app/src/app/app-routing.module.ts
@@ -1,28 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomePageComponent } from './Components/home-page/home-page.component';
-import { KitchenSinkAppComponent } from './Components/kitchen-sink-app/kitchen-sink-app.component';
-import { UserListPageComponent } from './Components/user-list-page/user-list-page.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomePageComponent } from "./Components/home-page/home-page.component";
+import { KitchenSinkAppComponent } from "./Components/kitchen-sink-app/kitchen-sink-app.component";
+import { CometChatConversationListPageComponent } from "./Components/Pages/comet-chat-conversation-list-page/comet-chat-conversation-list-page.component";
+import { UserListPageComponent } from "./Components/user-list-page/user-list-page.component";
 
 const routes: Routes = [
   {
-    path: 'login',
-    component: KitchenSinkAppComponent
+    path: "login",
+    component: KitchenSinkAppComponent,
   },
   {
-    path: 'Home',
-    component: HomePageComponent
+    path: "Home",
+    component: HomePageComponent,
   },
   {
-    path: 'contact-list',
-    component: UserListPageComponent
+    path: "contact-list",
+    component: UserListPageComponent,
   },
-
+  {
+    path: "conversation",
+    component: CometChatConversationListPageComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
