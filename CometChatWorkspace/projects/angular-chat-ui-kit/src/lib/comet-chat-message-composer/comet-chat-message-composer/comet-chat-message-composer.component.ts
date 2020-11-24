f1237d4498d30ce92c0fa7655610be07fea4bea3
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-24 14:07:03 +0530

    angular-chat-message-composer added

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-message-composer/comet-chat-message-composer/comet-chat-message-composer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-comet-chat-message-composer',
+  templateUrl: './comet-chat-message-composer.component.html',
+  styleUrls: ['./comet-chat-message-composer.component.css']
+})
+export class CometChatMessageComposerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
