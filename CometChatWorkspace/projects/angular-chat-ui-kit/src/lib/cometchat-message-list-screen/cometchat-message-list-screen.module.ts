34cbfb90ed81b67dfa20ab87425107d55e0f3c09
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-25 17:26:12 +0530

    message-list module added

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-message-list-screen/cometchat-message-list-screen.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-message-list-screen/cometchat-message-list-screen.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometchatMessageListScreenComponent } from "./cometchat-message-list-screen/cometchat-message-list-screen.component";
 import { MessageHeaderModule } from "../message-header/message-header.module";
+import { MessageListModule } from "../message-list/message-list.module";
 
 @NgModule({
   declarations: [CometchatMessageListScreenComponent],
-  imports: [CommonModule, MessageHeaderModule],
+  imports: [CommonModule, MessageHeaderModule, MessageListModule],
   exports: [CometchatMessageListScreenComponent],
 })
 export class CometchatMessageListScreenModule {}
