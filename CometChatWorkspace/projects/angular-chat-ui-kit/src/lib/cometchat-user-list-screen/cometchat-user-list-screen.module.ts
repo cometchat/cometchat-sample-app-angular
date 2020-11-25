6314adbd125cfa8f699034deb73676d6cb9c0727
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-11-25 13:15:03 +0530

    messageListScreen integrated in UserList Screen

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-user-list-screen/cometchat-user-list-screen.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-user-list-screen/cometchat-user-list-screen.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometchatUserListScreenComponent } from "./cometchat-user-list-screen/cometchat-user-list-screen.component";
 import { CometChatUserContactListModule } from "../comet-chat-user-contact-list/comet-chat-user-contact-list.module";
+import { CometchatMessageListScreenModule } from "../cometchat-message-list-screen/cometchat-message-list-screen.module";
 
 @NgModule({
   declarations: [CometchatUserListScreenComponent],
-  imports: [CommonModule, CometChatUserContactListModule],
+  imports: [
+    CommonModule,
+    CometChatUserContactListModule,
+    CometchatMessageListScreenModule,
+  ],
   exports: [CometchatUserListScreenComponent],
 })
 export class CometchatUserListScreenModule {}
