12d0a7cf380eda44b2b9ac876387e28626e85731
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-25 19:20:01 +0530

    dummy array created

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list/message-list.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list/message-list.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'lib-message-list',
-  templateUrl: './message-list.component.html',
-  styleUrls: ['./message-list.component.css']
+  selector: "lib-message-list",
+  templateUrl: "./message-list.component.html",
+  styleUrls: ["./message-list.component.css"],
 })
 export class MessageListComponent implements OnInit {
+  item = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
