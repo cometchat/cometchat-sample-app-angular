636378e53ee5c68df84c950c9b2ee93cf3569b28
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-25 15:27:42 +0530

    message-list created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessageListComponent } from './message-list/message-list.component';
+
+
+
+@NgModule({
+  declarations: [MessageListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MessageListModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list/message-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-message-list',
+  templateUrl: './message-list.component.html',
+  styleUrls: ['./message-list.component.css']
+})
+export class MessageListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
