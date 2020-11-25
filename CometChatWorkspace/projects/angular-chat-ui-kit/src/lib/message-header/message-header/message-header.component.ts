3340a4f1441ef904a4a456e4a9a67e856725e72d
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-11-25 14:44:02 +0530

    passing User Clicked Information from   MessageHeader To Avatar

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-header/message-header/message-header.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-header/message-header/message-header.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 @Component({
   selector: "lib-message-header",
   templateUrl: "./message-header.component.html",
   styleUrls: ["./message-header.component.css"],
 })
 export class MessageHeaderComponent implements OnInit {
+  @Input() item = null;
+
   constructor() {}
 
   ngOnInit() {}
