968e17ef99796588fb5397f8bcdea2333b986206
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-25 17:26:58 +0530

    sender and reciever message bubble modules imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MessageListComponent } from "./message-list/message-list.component";
+import { CometChatSenderMessageBubbleModule } from "../comet-chat-sender-message-bubble/comet-chat-sender-message-bubble.module";
+import { CometChatReceiverMessageBubbleModule } from "../comet-chat-receiver-message-bubble/comet-chat-receiver-message-bubble.module";
 
 @NgModule({
   declarations: [MessageListComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CometChatSenderMessageBubbleModule,
+    CometChatReceiverMessageBubbleModule,
+  ],
   exports: [MessageListComponent],
 })
 export class MessageListModule {}
