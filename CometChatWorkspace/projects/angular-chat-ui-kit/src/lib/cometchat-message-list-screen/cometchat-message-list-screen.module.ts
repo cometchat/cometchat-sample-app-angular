a4177ea5749919d8cf61580d927ab50f87a4f969
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-11-25 16:37:34 +0530

    integrated messageComposer in MessageListScreen

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-message-list-screen/cometchat-message-list-screen.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-message-list-screen/cometchat-message-list-screen.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometchatMessageListScreenComponent } from "./cometchat-message-list-screen/cometchat-message-list-screen.component";
 import { MessageHeaderModule } from "../message-header/message-header.module";
+import { CometChatMessageComposerModule } from "../comet-chat-message-composer/comet-chat-message-composer.module";
 
 @NgModule({
   declarations: [CometchatMessageListScreenComponent],
-  imports: [CommonModule, MessageHeaderModule],
+  imports: [CommonModule, MessageHeaderModule, CometChatMessageComposerModule],
   exports: [CometchatMessageListScreenComponent],
 })
 export class CometchatMessageListScreenModule {}
