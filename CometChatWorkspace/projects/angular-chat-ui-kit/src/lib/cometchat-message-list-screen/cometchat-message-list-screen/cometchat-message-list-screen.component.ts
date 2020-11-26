4147ecd1350ca9a4bed804348b4de75159e4b6b2
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-11-26 14:33:12 +0530

    action Handling added in messageListScreen

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-message-list-screen/cometchat-message-list-screen/cometchat-message-list-screen.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-message-list-screen/cometchat-message-list-screen/cometchat-message-list-screen.component.ts
@@ -7,8 +7,20 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class CometchatMessageListScreenComponent implements OnInit {
   @Input() item = null;
+  @Input() type = null;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    //console.log("MessageListScreen -> Type of User ", this.type);
+  }
+
+  /**
+   * Edit and Sent a Text message
+   * @param Event e
+   */
+  actionHandler(e) {
+    //handle Events/Actions generated from MessageHeader , MessageComposer and MessageList Here
+    console.log("MessageListScreen --> action generation is ", e);
+  }
 }
