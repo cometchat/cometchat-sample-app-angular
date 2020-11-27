d9574faf09f1dcdc62957ba5c49f8480a8d06675
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-27 18:26:13 +0530

    read-reciept created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/read-reciept/read-reciept.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReadRecieptComponent } from './read-reciept/read-reciept.component';
+
+
+
+@NgModule({
+  declarations: [ReadRecieptComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReadRecieptModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/read-reciept/read-reciept/read-reciept.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'read-reciept',
+  templateUrl: './read-reciept.component.html',
+  styleUrls: ['./read-reciept.component.css']
+})
+export class ReadRecieptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
