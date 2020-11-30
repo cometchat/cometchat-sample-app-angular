f175224d15d5a523328b7bb39eeff9417783098e
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-30 21:01:34 +0530

    getTime implemented also name and url variables used

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-file-bubble/receiver-file-bubble/receiver-file-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-file-bubble/receiver-file-bubble/receiver-file-bubble.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'receiver-file-bubble',
-  templateUrl: './receiver-file-bubble.component.html',
-  styleUrls: ['./receiver-file-bubble.component.css']
+  selector: "receiver-file-bubble",
+  templateUrl: "./receiver-file-bubble.component.html",
+  styleUrls: ["./receiver-file-bubble.component.css"],
 })
 export class ReceiverFileBubbleComponent implements OnInit {
-
-  constructor() { }
+  @Input() MessageDetails = null;
+  name: string;
+  url: string;
+  constructor() {}
 
   ngOnInit() {
+    this.name = this.MessageDetails.data.attachments[0].name;
+    this.url = this.MessageDetails.data.attachments[0].url;
   }
-
+  getTime() {
+    let msgSentAt = this.MessageDetails.sentAt;
+    let timeStamp = new Date(msgSentAt * 1000).toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    return timeStamp;
+  }
 }
