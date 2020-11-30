7cc86f9ea284596c2a4f3679bdff93996bdb94e2
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-30 19:46:19 +0530

    @Input added and url1 initialised

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-file-bubble/sender-file-bubble/sender-file-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-file-bubble/sender-file-bubble/sender-file-bubble.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'sender-file-bubble',
-  templateUrl: './sender-file-bubble.component.html',
-  styleUrls: ['./sender-file-bubble.component.css']
+  selector: "sender-file-bubble",
+  templateUrl: "./sender-file-bubble.component.html",
+  styleUrls: ["./sender-file-bubble.component.css"],
 })
 export class SenderFileBubbleComponent implements OnInit {
-
-  constructor() { }
+  @Input() MessageDetails = null;
+  url1;
+  constructor() {}
 
   ngOnInit() {
+    console.log("sender-file-bubble ->>> ", this.MessageDetails);
+    console.log(
+      "image detials ->>>> ",
+      this.MessageDetails.data.attachments[0].name
+    );
+    this.url1 = this.MessageDetails.data.attachments[0].url;
+    console.log(
+      "url detials ->>>> ",
+      this.MessageDetails.data.attachments[0].url
+    );
+    console.log("->>>>>>>>>>>>", this.url1);
   }
-
 }
