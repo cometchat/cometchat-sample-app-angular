4979899480ca588884655e86b584b08383794375
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-30 20:58:27 +0530

    receiver module added

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list.module.ts
@@ -4,6 +4,7 @@ import { MessageListComponent } from "./message-list/message-list.component";
 import { CometChatSenderMessageBubbleModule } from "../comet-chat-sender-message-bubble/comet-chat-sender-message-bubble.module";
 import { CometChatReceiverMessageBubbleModule } from "../comet-chat-receiver-message-bubble/comet-chat-receiver-message-bubble.module";
 import { SenderFileBubbleModule } from "../sender-file-bubble/sender-file-bubble.module";
+import { ReceiverFileBubbleModule } from "../receiver-file-bubble/receiver-file-bubble.module";
 
 @NgModule({
   declarations: [MessageListComponent],
@@ -11,6 +12,7 @@ import { SenderFileBubbleModule } from "../sender-file-bubble/sender-file-bubble
     CommonModule,
     CometChatSenderMessageBubbleModule,
     SenderFileBubbleModule,
+    ReceiverFileBubbleModule,
     CometChatReceiverMessageBubbleModule,
   ],
   exports: [MessageListComponent],
