e37612093e20765bf78a8d684a3abeee960f4a8e
Deepanshu Goyal <deepanshug@geekyants.com>
2020-11-30 20:03:31 +0530

    receiver-file-bubble created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-file-bubble/receiver-file-bubble.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReceiverFileBubbleComponent } from './receiver-file-bubble/receiver-file-bubble.component';
+
+
+
+@NgModule({
+  declarations: [ReceiverFileBubbleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReceiverFileBubbleModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-file-bubble/receiver-file-bubble/receiver-file-bubble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'receiver-file-bubble',
+  templateUrl: './receiver-file-bubble.component.html',
+  styleUrls: ['./receiver-file-bubble.component.css']
+})
+export class ReceiverFileBubbleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
