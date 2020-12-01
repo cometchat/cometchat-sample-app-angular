a643d3f3ea21aa100b24c66d470727bc3d179ac3
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-01 15:40:17 +0530

    messageComposer and MessageList imported in messageThread

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-thread/message-thread.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-thread/message-thread.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MessageThreadComponent } from "./message-thread/message-thread.component";
+import { CometChatSenderMessageBubbleModule } from "../comet-chat-sender-message-bubble/comet-chat-sender-message-bubble.module";
+import { MessageListModule } from "../message-list/message-list.module";
+import { CometChatMessageComposerModule } from "../comet-chat-message-composer/comet-chat-message-composer.module";
 
 @NgModule({
   declarations: [MessageThreadComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CometChatSenderMessageBubbleModule,
+    MessageListModule,
+    CometChatMessageComposerModule,
+  ],
   exports: [MessageThreadComponent],
 })
 export class MessageThreadModule {}
