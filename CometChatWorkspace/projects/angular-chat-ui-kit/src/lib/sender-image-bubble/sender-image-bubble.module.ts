27ece2190bcd8c9e9169375a14783cd87bebf810
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-01 13:58:21 +0530

    sender-image-bubble created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-image-bubble/sender-image-bubble.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SenderImageBubbleComponent } from './sender-image-bubble/sender-image-bubble.component';
+
+
+
+@NgModule({
+  declarations: [SenderImageBubbleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SenderImageBubbleModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-image-bubble/sender-image-bubble/sender-image-bubble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sender-image-bubble',
+  templateUrl: './sender-image-bubble.component.html',
+  styleUrls: ['./sender-image-bubble.component.css']
+})
+export class SenderImageBubbleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
