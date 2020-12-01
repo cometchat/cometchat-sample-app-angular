b26c220143ad423fa69bbb35e4bced64639a0a71
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-01 16:17:06 +0530

    close threadView action added

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-thread/message-thread/message-thread.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-thread/message-thread/message-thread.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'message-thread',
-  templateUrl: './message-thread.component.html',
-  styleUrls: ['./message-thread.component.css']
+  selector: "message-thread",
+  templateUrl: "./message-thread.component.html",
+  styleUrls: ["./message-thread.component.css"],
 })
 export class MessageThreadComponent implements OnInit {
+  dummyMessage = {
+    data: {
+      text: "testing thread",
+    },
+    sender: {
+      sentAt: 1606742046,
+    },
+  };
 
-  constructor() { }
+  @Input() item = null;
+  @Input() type = null;
+  @Input() parentMessage = null;
 
-  ngOnInit() {
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  /**
+   * Action is Generated to inform UserListScreen to close the thread window
+   * @param
+   */
+  closeThread() {
+    console.log("close thread clicked");
+    this.actionGenerated.emit({ type: "closeThreadClicked", payLoad: null });
   }
-
 }
