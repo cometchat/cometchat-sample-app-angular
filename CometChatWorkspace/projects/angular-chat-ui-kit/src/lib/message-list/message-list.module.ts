1f87e42a12c331c3dc0f4443b9c2f4710cbf7f7e
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-01 16:14:57 +0530

    sender-image-bubble module imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/message-list/message-list.module.ts
@@ -5,6 +5,7 @@ import { CometChatSenderMessageBubbleModule } from "../comet-chat-sender-message
 import { CometChatReceiverMessageBubbleModule } from "../comet-chat-receiver-message-bubble/comet-chat-receiver-message-bubble.module";
 import { SenderFileBubbleModule } from "../sender-file-bubble/sender-file-bubble.module";
 import { ReceiverFileBubbleModule } from "../receiver-file-bubble/receiver-file-bubble.module";
+import { SenderImageBubbleModule } from "../sender-image-bubble/sender-image-bubble.module";
 
 @NgModule({
   declarations: [MessageListComponent],
@@ -13,6 +14,7 @@ import { ReceiverFileBubbleModule } from "../receiver-file-bubble/receiver-file-
     CometChatSenderMessageBubbleModule,
     SenderFileBubbleModule,
     ReceiverFileBubbleModule,
+    SenderImageBubbleModule,
     CometChatReceiverMessageBubbleModule,
   ],
   exports: [MessageListComponent],
