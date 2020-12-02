e772a98186451cf365b0a9bab49d074414ebd68a
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-02 12:23:36 +0530

    functions created to display image

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-image-bubble/receiver-image-bubble/receiver-image-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-image-bubble/receiver-image-bubble/receiver-image-bubble.component.ts
@@ -1,15 +1,111 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'receiver-image-bubble',
-  templateUrl: './receiver-image-bubble.component.html',
-  styleUrls: ['./receiver-image-bubble.component.css']
+  selector: "receiver-image-bubble",
+  templateUrl: "./receiver-image-bubble.component.html",
+  styleUrls: ["./receiver-image-bubble.component.css"],
 })
 export class ReceiverImageBubbleComponent implements OnInit {
+  @Input() MessageDetails = null;
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
+  messageFrom = "receiver";
 
-  constructor() { }
+  messageAssign = Object.assign({}, this.MessageDetails, {
+    messageFrom: this.messageFrom,
+  });
+
+  message = this.messageAssign;
+  imageUrl = "";
+
+  constructor() {}
 
   ngOnInit() {
+    if (this.MessageDetails.receiverType === "group") {
+      if (!this.MessageDetails.sender.avatar) {
+        const uid = this.MessageDetails.sender.getUid();
+        const char = this.MessageDetails.sender
+          .getName()
+          .charAt(0)
+          .toUpperCase();
+        // this.MessageDetails.sender.setAvatar(SvgAvatar.getAvatar(uid, char));
+      }
+    }
+    this.setImage();
+  }
+  setImage() {
+    const metadata = this.MessageDetails.metadata;
+    const injectedObject = metadata["@injected"];
+    if (injectedObject && injectedObject.hasOwnProperty("extensions")) {
+      const extensionsObject = injectedObject["extensions"];
+      if (
+        extensionsObject &&
+        extensionsObject.hasOwnProperty("thumbnail-generation")
+      ) {
+        const thumbnailGenerationObject =
+          extensionsObject["thumbnail-generation"];
+        //mq harcoded value is used until theme is not passed change it after
+        const mq = window.matchMedia(
+          "(min-width:360px) and (max-width: 767px)"
+        );
+
+        //when theme is passed use this mq
+        //const mq = window.matchMedia(this.MessageDetails.theme.breakPoints[0]);
+
+        mq.addListener(() => {
+          const imageToDownload = this.chooseImage(thumbnailGenerationObject);
+          let img = new Image();
+          img.src = imageToDownload;
+          img.onload = () => {
+            this.imageUrl = img.src;
+            console.log("eventlist");
+          };
+        });
+        const imageToDownload = this.chooseImage(thumbnailGenerationObject);
+        let img = new Image();
+        img.src = imageToDownload;
+        img.onload = () => {
+          this.imageUrl = img.src;
+          URL.revokeObjectURL(img.src);
+        };
+      }
+    } else {
+      this.setMessageImageUrl();
+    }
+  }
+  setMessageImageUrl = () => {
+    let img = new Image();
+    img.src = this.MessageDetails.data.url;
+    img.onload = () => {
+      this.imageUrl = img.src;
+    };
+  };
+
+  chooseImage(thumbnailGenerationObject) {
+    const smallUrl = thumbnailGenerationObject["url_small"];
+    const mediumUrl = thumbnailGenerationObject["url_medium"];
+    const mq = window.matchMedia("(min-width:360px) and (max-width: 767px)");
+    let imageToDownload = mediumUrl;
+    if (mq.matches) {
+      imageToDownload = smallUrl;
+    }
+
+    return imageToDownload;
   }
 
+  open() {
+    this.actionGenerated.emit({
+      type: "viewActualImage",
+      payLoad: { ...this.message, ...this.MessageDetails },
+    });
+  }
+
+  getTime() {
+    let msgSentAt = this.MessageDetails.sentAt;
+    let timeStamp = new Date(msgSentAt * 1000).toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    return timeStamp;
+  }
 }
