895e2c125b814afe3eb14f0666a986af626b1cb8
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-02 18:50:09 +0530

    tooltip component integrated

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-sender-message-bubble/comet-chat-sender-message-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-sender-message-bubble/comet-chat-sender-message-bubble.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometChatSenderMessageBubbleComponent } from "./comet-chat-sender-message-bubble/comet-chat-sender-message-bubble.component";
 import { ReadRecieptModule } from "../read-reciept/read-reciept.module";
+import { ToolTipModule } from "../tool-tip/tool-tip.module";
 
 @NgModule({
   declarations: [CometChatSenderMessageBubbleComponent],
-  imports: [CommonModule, ReadRecieptModule],
+  imports: [CommonModule, ReadRecieptModule, ToolTipModule],
   exports: [CometChatSenderMessageBubbleComponent],
 })
 export class CometChatSenderMessageBubbleModule {}
