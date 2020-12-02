8d1bdbb117ef32ecce3c68eda6123f8ef113de56
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-02 15:10:02 +0530

    function created to get umage link and to close full screen

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/image-view/image-view/image-view.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/image-view/image-view/image-view.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'image-view',
-  templateUrl: './image-view.component.html',
-  styleUrls: ['./image-view.component.css']
+  selector: "image-view",
+  templateUrl: "./image-view.component.html",
+  styleUrls: ["./image-view.component.css"],
 })
 export class ImageViewComponent implements OnInit {
+  @Input() MessageDetails;
+  @Input() open;
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  imageUrl: string;
+  constructor() {}
 
   ngOnInit() {
+    this.getUrl();
+  }
+  getUrl() {
+    // console.log("dei", this.MessageDetails);
+
+    let img = new Image();
+    img.src = this.MessageDetails.data.url;
+    this.imageUrl = img.src;
   }
 
+  close() {
+    this.actionGenerated.emit({
+      type: "closeFullScreenImage",
+    });
+  }
 }
