3a09b7f6479193c491829c7cff51acfd211f8fe5
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-02 12:21:53 +0530

    receiver-image-bubble.component exported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-image-bubble/receiver-image-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-image-bubble/receiver-image-bubble.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReceiverImageBubbleComponent } from './receiver-image-bubble/receiver-image-bubble.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReceiverImageBubbleComponent } from "./receiver-image-bubble/receiver-image-bubble.component";
 
 @NgModule({
   declarations: [ReceiverImageBubbleComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [ReceiverImageBubbleComponent],
 })
-export class ReceiverImageBubbleModule { }
+export class ReceiverImageBubbleModule {}
