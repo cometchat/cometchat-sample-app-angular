db54152ca85ffb68e2da84caa4994a8e6eb81b7c
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-02 18:11:22 +0530

    getUrl function created to get url

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-video-bubble/sender-video-bubble/sender-video-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-video-bubble/sender-video-bubble/sender-video-bubble.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'sender-video-bubble',
-  templateUrl: './sender-video-bubble.component.html',
-  styleUrls: ['./sender-video-bubble.component.css']
+  selector: "sender-video-bubble",
+  templateUrl: "./sender-video-bubble.component.html",
+  styleUrls: ["./sender-video-bubble.component.css"],
 })
 export class SenderVideoBubbleComponent implements OnInit {
+  @Input() MessageDetails = null;
+  videoUrl: string;
+  messageFrom = "sender";
 
-  constructor() { }
+  message = Object.assign({}, this.MessageDetails, {
+    messageFrom: this.messageFrom,
+  });
+
+  constructor() {}
 
   ngOnInit() {
+    this.getUrl();
   }
 
+  getUrl() {
+    this.videoUrl = this.MessageDetails.data.url;
+  }
 }
