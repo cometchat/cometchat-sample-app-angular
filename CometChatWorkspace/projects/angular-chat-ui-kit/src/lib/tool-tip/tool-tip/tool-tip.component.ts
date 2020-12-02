c76ccdd6d1861a105b73bdd214571ee5dae021ec
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-02 12:46:57 +0530

    edit , reply , delete actions added in tooltip

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/tool-tip/tool-tip/tool-tip.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/tool-tip/tool-tip/tool-tip.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'tool-tip',
-  templateUrl: './tool-tip.component.html',
-  styleUrls: ['./tool-tip.component.css']
+  selector: "tool-tip",
+  templateUrl: "./tool-tip.component.html",
+  styleUrls: ["./tool-tip.component.css"],
 })
 export class ToolTipComponent implements OnInit {
+  @Input() MessageDetails = null;
 
-  constructor() { }
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  /**
+   * Generates an action to reply to the current message
+   *
+   */
+  replyToMessage() {
+    console.log("reply to ", this.MessageDetails);
+    this.actionGenerated.emit({
+      type: "viewMessageThread",
+      payLoad: this.MessageDetails,
+    });
   }
 
+  /**
+   * Generates an action to edit  the current message
+   *
+   */
+  editMessage() {
+    console.log("edit message ", this.MessageDetails);
+    this.actionGenerated.emit({
+      type: "editMessage",
+      payLoad: this.MessageDetails,
+    });
+  }
+
+  /**
+   * Generates an action to Delete  the current message
+   *
+   */
+  deleteMessage() {
+    console.log("Delete ", this.MessageDetails);
+    this.actionGenerated.emit({
+      type: "deleteMessage",
+      payLoad: this.MessageDetails,
+    });
+  }
 }
