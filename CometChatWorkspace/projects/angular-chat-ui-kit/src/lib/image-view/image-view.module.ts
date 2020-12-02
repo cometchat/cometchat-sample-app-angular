77381899a7b77538a9a8bb6048fdd90a6963c9c9
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-02 13:31:10 +0530

    component created to display image in full screen

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/image-view/image-view.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ImageViewComponent } from './image-view/image-view.component';
+
+
+
+@NgModule({
+  declarations: [ImageViewComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ImageViewModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/image-view/image-view/image-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'image-view',
+  templateUrl: './image-view.component.html',
+  styleUrls: ['./image-view.component.css']
+})
+export class ImageViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
