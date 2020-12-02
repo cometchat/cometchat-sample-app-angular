4e409dc9facc42bce524cef36066fe284c65cf5b
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-02 17:32:33 +0530

    sender-video-bubble created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-video-bubble/sender-video-bubble.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SenderVideoBubbleComponent } from './sender-video-bubble/sender-video-bubble.component';
+
+
+
+@NgModule({
+  declarations: [SenderVideoBubbleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SenderVideoBubbleModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-video-bubble/sender-video-bubble/sender-video-bubble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sender-video-bubble',
+  templateUrl: './sender-video-bubble.component.html',
+  styleUrls: ['./sender-video-bubble.component.css']
+})
+export class SenderVideoBubbleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
