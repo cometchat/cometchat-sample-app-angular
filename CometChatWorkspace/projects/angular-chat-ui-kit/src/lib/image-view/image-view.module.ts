1460add1737c8db7a24ed04b61992333a27bb565
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-02 15:00:02 +0530

    image-view component exported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/image-view/image-view.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/image-view/image-view.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ImageViewComponent } from './image-view/image-view.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ImageViewComponent } from "./image-view/image-view.component";
 
 @NgModule({
   declarations: [ImageViewComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [ImageViewComponent],
 })
-export class ImageViewModule { }
+export class ImageViewModule {}
