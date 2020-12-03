87dcad42b5fb97b557fe4e09b5e51e8e03a5ca5e
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-03 15:41:01 +0530

    imported tooltip and avatar module and exported receiver-audio-bubble-component

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-audio-bubble/receiver-audio-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-audio-bubble/receiver-audio-bubble.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReceiverAudioBubbleComponent } from './receiver-audio-bubble/receiver-audio-bubble.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReceiverAudioBubbleComponent } from "./receiver-audio-bubble/receiver-audio-bubble.component";
+import { ToolTipModule } from "../tool-tip/tool-tip.module";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [ReceiverAudioBubbleComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, ToolTipModule, AvatarModule],
+  exports: [ReceiverAudioBubbleComponent],
 })
-export class ReceiverAudioBubbleModule { }
+export class ReceiverAudioBubbleModule {}
