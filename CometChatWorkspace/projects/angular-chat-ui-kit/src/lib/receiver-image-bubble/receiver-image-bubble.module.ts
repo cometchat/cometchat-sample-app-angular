da5bc2e9cb66ae2d3bbbb75df4501ecbf967002a
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-03 16:58:36 +0530

    imported tooltip and avatar module

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-image-bubble/receiver-image-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-image-bubble/receiver-image-bubble.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReceiverImageBubbleComponent } from "./receiver-image-bubble/receiver-image-bubble.component";
+import { ToolTipModule } from "../tool-tip/tool-tip.module";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [ReceiverImageBubbleComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ToolTipModule, AvatarModule],
   exports: [ReceiverImageBubbleComponent],
 })
 export class ReceiverImageBubbleModule {}
