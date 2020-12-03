0427cb5027506a4192fd9fd8b864502614892ec9
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-03 17:10:26 +0530

    added action handler

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-file-bubble/sender-file-bubble/sender-file-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-file-bubble/sender-file-bubble/sender-file-bubble.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "sender-file-bubble",
@@ -7,6 +7,8 @@ import { Component, Input, OnInit } from "@angular/core";
 })
 export class SenderFileBubbleComponent implements OnInit {
   @Input() MessageDetails = null;
+  @Input() showToolTip = true;
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
   url: string;
   name: string;
   constructor() {}
@@ -14,5 +16,13 @@ export class SenderFileBubbleComponent implements OnInit {
   ngOnInit() {
     this.url = this.MessageDetails.data.attachments[0].url;
     this.name = this.MessageDetails.data.attachments[0].name;
+  }
+  /**
+   * Handles all the actions emitted by the child components that make the current component
+   * @param Event action
+   */
+  actionHandler(action) {
+    console.log("receiver Message Bubble --> action generation is ", action);
+    this.actionGenerated.emit(action);
   }
 }
