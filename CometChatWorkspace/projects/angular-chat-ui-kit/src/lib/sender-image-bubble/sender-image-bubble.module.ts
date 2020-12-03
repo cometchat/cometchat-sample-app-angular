768f005c95b522254c4dbe3956d6d4f1b16c99a0
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-03 14:00:27 +0530

    imported tootip

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-image-bubble/sender-image-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-image-bubble/sender-image-bubble.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SenderImageBubbleComponent } from "./sender-image-bubble/sender-image-bubble.component";
 import { ReadRecieptModule } from "../read-reciept/read-reciept.module";
+import { ToolTipModule } from "../tool-tip/tool-tip.module";
 
 @NgModule({
   declarations: [SenderImageBubbleComponent],
-  imports: [CommonModule, ReadRecieptModule],
+  imports: [CommonModule, ReadRecieptModule, ToolTipModule],
   exports: [SenderImageBubbleComponent],
 })
 export class SenderImageBubbleModule {}
