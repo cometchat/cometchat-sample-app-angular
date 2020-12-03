bcd3f797d216225981a2624b88102b442b46bd08
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-03 14:25:31 +0530

    created function to get audioUrl and to toggle thread

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-audio-bubble/sender-audio-bubble/sender-audio-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-audio-bubble/sender-audio-bubble/sender-audio-bubble.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'sender-audio-bubble',
-  templateUrl: './sender-audio-bubble.component.html',
-  styleUrls: ['./sender-audio-bubble.component.css']
+  selector: "sender-audio-bubble",
+  templateUrl: "./sender-audio-bubble.component.html",
+  styleUrls: ["./sender-audio-bubble.component.css"],
 })
 export class SenderAudioBubbleComponent implements OnInit {
+  @Input() MessageDetails = null;
+  @Input() showToolTip = true;
 
-  constructor() { }
+  audioUrl: string;
+  message = Object.assign({}, this.MessageDetails, { messageFrom: "sender" });
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
 
   ngOnInit() {
+    this.getUrl();
   }
-
+  getUrl() {
+    this.audioUrl = this.MessageDetails.data.url;
+  }
+  /**
+   * Handles all the actions emitted by the child components that make the current component
+   * @param Event action
+   */
+  actionHandler(action) {
+    console.log("receiver Message Bubble --> action generation is ", action);
+    this.actionGenerated.emit(action);
+  }
 }
