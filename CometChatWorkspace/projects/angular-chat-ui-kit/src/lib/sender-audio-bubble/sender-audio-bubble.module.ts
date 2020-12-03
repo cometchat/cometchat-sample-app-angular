f80e5ac6ba95df5a571f95b0cf3629100c060cc9
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-03 14:06:03 +0530

    sender-audio-bubble created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-audio-bubble/sender-audio-bubble.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SenderAudioBubbleComponent } from './sender-audio-bubble/sender-audio-bubble.component';
+
+
+
+@NgModule({
+  declarations: [SenderAudioBubbleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SenderAudioBubbleModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-audio-bubble/sender-audio-bubble/sender-audio-bubble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sender-audio-bubble',
+  templateUrl: './sender-audio-bubble.component.html',
+  styleUrls: ['./sender-audio-bubble.component.css']
+})
+export class SenderAudioBubbleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
