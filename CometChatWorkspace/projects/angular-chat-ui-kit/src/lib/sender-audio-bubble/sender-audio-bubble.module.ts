de45f4a4cdfaaead59cbb73df05e97160965a765
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-03 14:24:19 +0530

    imported read-receipt and tooltip and exported sender-audio-bubble component

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-audio-bubble/sender-audio-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-audio-bubble/sender-audio-bubble.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SenderAudioBubbleComponent } from './sender-audio-bubble/sender-audio-bubble.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SenderAudioBubbleComponent } from "./sender-audio-bubble/sender-audio-bubble.component";
+import { ToolTipModule } from "../tool-tip/tool-tip.module";
+import { ReadRecieptModule } from "../read-reciept/read-reciept.module";
 
 @NgModule({
   declarations: [SenderAudioBubbleComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, ToolTipModule, ReadRecieptModule],
+  exports: [SenderAudioBubbleComponent],
 })
-export class SenderAudioBubbleModule { }
+export class SenderAudioBubbleModule {}
