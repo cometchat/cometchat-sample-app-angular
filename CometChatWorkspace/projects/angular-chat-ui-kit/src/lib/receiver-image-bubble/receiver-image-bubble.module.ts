13cba038970e3bc4b2b62d7131817b6f41b182f9
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-04 21:37:48 +0530

    imported reply-count

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-image-bubble/receiver-image-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-image-bubble/receiver-image-bubble.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { ReceiverImageBubbleComponent } from "./receiver-image-bubble/receiver-image-bubble.component";
 import { ToolTipModule } from "../tool-tip/tool-tip.module";
 import { AvatarModule } from "../avatar/avatar.module";
+import { ReplyCountModule } from "../reply-count/reply-count.module";
 
 @NgModule({
   declarations: [ReceiverImageBubbleComponent],
-  imports: [CommonModule, ToolTipModule, AvatarModule],
+  imports: [CommonModule, ToolTipModule, AvatarModule, ReplyCountModule],
   exports: [ReceiverImageBubbleComponent],
 })
 export class ReceiverImageBubbleModule {}
