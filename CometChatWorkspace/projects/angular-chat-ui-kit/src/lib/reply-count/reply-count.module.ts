c00c671998b14b6ced044597a8ee883916240bd6
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-04 20:45:43 +0530

    reply-count created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/reply-count/reply-count.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReplyCountComponent } from './reply-count/reply-count.component';
+
+
+
+@NgModule({
+  declarations: [ReplyCountComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReplyCountModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/reply-count/reply-count/reply-count.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'reply-count',
+  templateUrl: './reply-count.component.html',
+  styleUrls: ['./reply-count.component.css']
+})
+export class ReplyCountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
