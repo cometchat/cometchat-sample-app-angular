a9e8dcc6fc40b1eb5d6ef3717ad07b0b27db849d
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-07 18:10:05 +0530

    shared-media-view module imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-user-detail/comet-chat-user-detail.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-user-detail/comet-chat-user-detail.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometChatUserDetailComponent } from "./comet-chat-user-detail/comet-chat-user-detail.component";
+import { SharedMediaViewModule } from "../shared-media-view/shared-media-view.module";
 
 @NgModule({
   declarations: [CometChatUserDetailComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedMediaViewModule],
   exports: [CometChatUserDetailComponent],
 })
 export class CometChatUserDetailModule {}
