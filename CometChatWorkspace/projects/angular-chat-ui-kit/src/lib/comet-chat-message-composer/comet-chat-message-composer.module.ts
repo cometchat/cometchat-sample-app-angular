db0c7f51fad2feb58f5e5f2749437efc4e6cf8c6
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-07 15:06:59 +0530

    reply-preview module import

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-message-composer/comet-chat-message-composer.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-message-composer/comet-chat-message-composer.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometChatMessageComposerComponent } from "./comet-chat-message-composer/comet-chat-message-composer.component";
+import { ReplyPreviewModule } from "../reply-preview/reply-preview.module";
 
 @NgModule({
   declarations: [CometChatMessageComposerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReplyPreviewModule],
   exports: [CometChatMessageComposerComponent],
 })
 export class CometChatMessageComposerModule {}
