a04dec8aa908537b528c4db31ff3751825e9ee8d
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-08 13:11:39 +0530

    emoji picker imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-message-composer/comet-chat-message-composer.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-message-composer/comet-chat-message-composer.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometChatMessageComposerComponent } from "./comet-chat-message-composer/comet-chat-message-composer.component";
 import { ReplyPreviewModule } from "../reply-preview/reply-preview.module";
-
+import { NgxEmojiPickerModule } from "ngx-emoji-picker";
 @NgModule({
   declarations: [CometChatMessageComposerComponent],
-  imports: [CommonModule, ReplyPreviewModule],
+  imports: [CommonModule, ReplyPreviewModule, NgxEmojiPickerModule],
   exports: [CometChatMessageComposerComponent],
 })
 export class CometChatMessageComposerModule {}
