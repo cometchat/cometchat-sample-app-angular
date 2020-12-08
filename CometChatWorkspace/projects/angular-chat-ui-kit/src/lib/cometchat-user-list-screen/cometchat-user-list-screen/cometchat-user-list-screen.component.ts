8db38e2dd973ca0f20abee726880d4f1b650ce82
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-08 11:57:32 +0530

    added checks so that thread and user-detail-screen closes when user switches chat window

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-user-list-screen/cometchat-user-list-screen/cometchat-user-list-screen.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-user-list-screen/cometchat-user-list-screen/cometchat-user-list-screen.component.ts
@@ -35,6 +35,10 @@ export class CometchatUserListScreenComponent implements OnInit {
     // console.log(`user in parent component  `, user);
     this.curentItem = user;
 
+    //Close Thread And User Detail Screen When Chat Window Is Changed
+    this.closeThreadMessages();
+    this.viewDetailScreen = false;
+
     if (this.curentItem.hasOwnProperty("uid")) {
       this.type = "user";
     } else {
@@ -122,6 +126,7 @@ export class CometchatUserListScreenComponent implements OnInit {
    * @param Any message
    */
   toggleDetailView = () => {
+    this.threadMessageView = false;
     this.viewDetailScreen = !this.viewDetailScreen;
   };
 }
