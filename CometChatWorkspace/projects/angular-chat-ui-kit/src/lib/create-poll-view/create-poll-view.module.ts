9ae1d617bab4ca97bb565c6b8e2aaab58615ce23
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-08 16:45:40 +0530

    poll styling added

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/create-poll-view/create-poll-view.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/create-poll-view/create-poll-view.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CreatePollViewComponent } from "./create-poll-view/create-poll-view.component";
+import { BackdropModule } from "../backdrop/backdrop.module";
 
 @NgModule({
   declarations: [CreatePollViewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, BackdropModule],
   exports: [CreatePollViewComponent],
 })
 export class CreatePollViewModule {}
