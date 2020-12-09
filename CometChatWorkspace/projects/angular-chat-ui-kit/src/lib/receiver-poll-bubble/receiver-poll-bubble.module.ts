9ad249f4b889444eda1de4f279c56a794ecc7d40
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-09 20:27:08 +0530

    removed extra imports

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReceiverPollBubbleComponent } from "./receiver-poll-bubble/receiver-poll-bubble.component";
 import { ToolTipModule } from "../tool-tip/tool-tip.module";
-import { ReadRecieptModule } from "../read-reciept/read-reciept.module";
 
 @NgModule({
   declarations: [ReceiverPollBubbleComponent],
-  imports: [CommonModule, ToolTipModule, ReadRecieptModule],
+  imports: [CommonModule, ToolTipModule],
   exports: [ReceiverPollBubbleComponent],
 })
 export class ReceiverPollBubbleModule {}
