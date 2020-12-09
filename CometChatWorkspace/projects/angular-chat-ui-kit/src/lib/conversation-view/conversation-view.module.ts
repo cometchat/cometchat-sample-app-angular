7a0e413c80289f29700800e9908c4168f1ef59e0
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-09 17:31:53 +0530

    created conversation-view

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/conversation-view/conversation-view.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConversationViewComponent } from './conversation-view/conversation-view.component';
+
+
+
+@NgModule({
+  declarations: [ConversationViewComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConversationViewModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/conversation-view/conversation-view/conversation-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'conversation-view',
+  templateUrl: './conversation-view.component.html',
+  styleUrls: ['./conversation-view.component.css']
+})
+export class ConversationViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
