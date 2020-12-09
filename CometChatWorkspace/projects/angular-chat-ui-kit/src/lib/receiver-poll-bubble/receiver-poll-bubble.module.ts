2e664bc522976e68a7a859bfbe4975c3c1ae83b2
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-09 14:54:22 +0530

    actions added receiver poll bubble

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from "@angular/common";
 import { ReceiverPollBubbleComponent } from "./receiver-poll-bubble/receiver-poll-bubble.component";
 import { ToolTipModule } from "../tool-tip/tool-tip.module";
 import { ReadRecieptModule } from "../read-reciept/read-reciept.module";
-import { ReplyCountModule } from "../reply-count/reply-count.module";
 
 @NgModule({
   declarations: [ReceiverPollBubbleComponent],
-  imports: [CommonModule, ToolTipModule, ReadRecieptModule, ReplyCountModule],
+  imports: [CommonModule, ToolTipModule, ReadRecieptModule],
   exports: [ReceiverPollBubbleComponent],
 })
 export class ReceiverPollBubbleModule {}
--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble/receiver-poll-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble/receiver-poll-bubble.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'receiver-poll-bubble',
-  templateUrl: './receiver-poll-bubble.component.html',
-  styleUrls: ['./receiver-poll-bubble.component.css']
+  selector: "receiver-poll-bubble",
+  templateUrl: "./receiver-poll-bubble.component.html",
+  styleUrls: ["./receiver-poll-bubble.component.css"],
 })
 export class ReceiverPollBubbleComponent implements OnInit {
+  @Input() MessageDetails = null;
+  @Input() showReplyCount = true;
 
-  constructor() { }
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit() {
+  @Input() showToolTip = true;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  /**
+   * Handles all the actions emitted by the child components that make the current component
+   * @param Event action
+   */
+  actionHandler(action) {
+    console.log("receiver Poll Bubble --> action generation is ", action);
+    this.actionGenerated.emit(action);
   }
-
 }
