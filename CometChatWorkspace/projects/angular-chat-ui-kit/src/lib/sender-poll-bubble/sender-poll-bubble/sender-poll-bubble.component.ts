58e463b06679ff28a07fdc84f569dc49313c0525
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-09 13:14:53 +0530

    sender poll bubble actions added

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-poll-bubble/sender-poll-bubble/sender-poll-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-poll-bubble/sender-poll-bubble/sender-poll-bubble.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'sender-poll-bubble',
-  templateUrl: './sender-poll-bubble.component.html',
-  styleUrls: ['./sender-poll-bubble.component.css']
+  selector: "sender-poll-bubble",
+  templateUrl: "./sender-poll-bubble.component.html",
+  styleUrls: ["./sender-poll-bubble.component.css"],
 })
 export class SenderPollBubbleComponent implements OnInit {
+  @Input() MessageDetails = null;
+  @Input() showReplyCount = true;
 
-  constructor() { }
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit() {
+  @Input() showToolTip = true;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  /**
+   * Handles all the actions emitted by the child components that make the current component
+   * @param Event action
+   */
+  actionHandler(action) {
+    console.log("Sender Poll Bubble --> action generation is ", action);
+    this.actionGenerated.emit(action);
   }
-
 }
