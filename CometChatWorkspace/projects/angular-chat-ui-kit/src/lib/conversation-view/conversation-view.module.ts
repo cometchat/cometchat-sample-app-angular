6343b6d22389c6a7cf15402753289dcaffa96bef
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-09 21:12:38 +0530

    exported component and imported Avatar Module

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/conversation-view/conversation-view.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/conversation-view/conversation-view.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ConversationViewComponent } from './conversation-view/conversation-view.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ConversationViewComponent } from "./conversation-view/conversation-view.component";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [ConversationViewComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, AvatarModule],
+  exports: [ConversationViewComponent],
 })
-export class ConversationViewModule { }
+export class ConversationViewModule {}
