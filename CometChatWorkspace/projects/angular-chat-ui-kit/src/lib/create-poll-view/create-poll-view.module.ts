2c628a823d8923e798b37d8de50aa7810b1474da
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-09 11:53:32 +0530

    poll View actions added

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/create-poll-view/create-poll-view.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/create-poll-view/create-poll-view.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CreatePollViewComponent } from "./create-poll-view/create-poll-view.component";
 import { BackdropModule } from "../backdrop/backdrop.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [CreatePollViewComponent],
-  imports: [CommonModule, BackdropModule],
+  imports: [CommonModule, BackdropModule, FormsModule, ReactiveFormsModule],
   exports: [CreatePollViewComponent],
 })
 export class CreatePollViewModule {}
