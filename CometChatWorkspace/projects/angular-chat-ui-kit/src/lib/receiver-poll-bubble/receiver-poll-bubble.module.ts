7af92b2a9432f6c1272b66321031ea3ae6d87d26
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-09 13:25:34 +0530

    ToolTipModule, ReadRecieptModule, ReplyCountModule imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReceiverPollBubbleComponent } from "./receiver-poll-bubble/receiver-poll-bubble.component";
+import { ToolTipModule } from "../tool-tip/tool-tip.module";
+import { ReadRecieptModule } from "../read-reciept/read-reciept.module";
+import { ReplyCountModule } from "../reply-count/reply-count.module";
 
 @NgModule({
   declarations: [ReceiverPollBubbleComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ToolTipModule, ReadRecieptModule, ReplyCountModule],
   exports: [ReceiverPollBubbleComponent],
 })
 export class ReceiverPollBubbleModule {}
