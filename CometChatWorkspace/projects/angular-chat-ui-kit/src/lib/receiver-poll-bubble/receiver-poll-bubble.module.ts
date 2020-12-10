1bbab548569afbb15aa17ab6693726f20aac7aa5
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-10 12:33:19 +0530

    removed tool tip from poll receiver bubble

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReceiverPollBubbleComponent } from "./receiver-poll-bubble/receiver-poll-bubble.component";
-import { ToolTipModule } from "../tool-tip/tool-tip.module";
 
 @NgModule({
   declarations: [ReceiverPollBubbleComponent],
-  imports: [CommonModule, ToolTipModule],
+  imports: [CommonModule],
   exports: [ReceiverPollBubbleComponent],
 })
 export class ReceiverPollBubbleModule {}
