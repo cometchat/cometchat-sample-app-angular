2b67d2cab8478d202e82fb7ee61c03b703196e89
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-10 19:38:13 +0530

    group view component imported in GroupList

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-group-list/comet-chat-group-list.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-group-list/comet-chat-group-list.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometChatGroupListComponent } from "./comet-chat-group-list/comet-chat-group-list.component";
+import { GroupViewModule } from "../group-view/group-view.module";
 
 @NgModule({
   declarations: [CometChatGroupListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, GroupViewModule],
   exports: [CometChatGroupListComponent],
 })
 export class CometChatGroupListModule {}
