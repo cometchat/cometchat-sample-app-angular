fa24b6d111f539e5aa92abc1547ec15dc7c70790
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-10 12:02:07 +0530

    created badge count

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/badge-count/badge-count.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BadgeCountComponent } from './badge-count/badge-count.component';
+
+
+
+@NgModule({
+  declarations: [BadgeCountComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BadgeCountModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/badge-count/badge-count/badge-count.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'badge-count',
+  templateUrl: './badge-count.component.html',
+  styleUrls: ['./badge-count.component.css']
+})
+export class BadgeCountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
