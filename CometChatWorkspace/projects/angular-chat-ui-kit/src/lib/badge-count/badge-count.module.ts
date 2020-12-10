68cbad0978585aff14bf53b8a2acf0606fa00a73
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-10 12:02:46 +0530

    exported badge-count component

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/badge-count/badge-count.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/badge-count/badge-count.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BadgeCountComponent } from './badge-count/badge-count.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BadgeCountComponent } from "./badge-count/badge-count.component";
 
 @NgModule({
   declarations: [BadgeCountComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [BadgeCountComponent],
 })
-export class BadgeCountModule { }
+export class BadgeCountModule {}
