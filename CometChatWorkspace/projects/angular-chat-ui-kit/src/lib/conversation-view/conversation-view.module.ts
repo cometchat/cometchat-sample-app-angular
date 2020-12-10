bd871ae7a00248c451fd26a5a89d460b58111e57
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-10 12:55:47 +0530

    badge-count module imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/conversation-view/conversation-view.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/conversation-view/conversation-view.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ConversationViewComponent } from "./conversation-view/conversation-view.component";
 import { AvatarModule } from "../avatar/avatar.module";
+import { BadgeCountModule } from "../badge-count/badge-count.module";
 
 @NgModule({
   declarations: [ConversationViewComponent],
-  imports: [CommonModule, AvatarModule],
+  imports: [CommonModule, AvatarModule, BadgeCountModule],
   exports: [ConversationViewComponent],
 })
 export class ConversationViewModule {}
