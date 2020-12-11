2ba00a40b312673afc98337c69759e0720a10e8f
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-11 13:48:05 +0530

    added routing conversation-screen

--- CometChatWorkspace/projects/angular-chat-app/src/app/app-routing.module.ts
+++ CometChatWorkspace/projects/angular-chat-app/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomePageComponent } from "./Components/home-page/home-page.component";
 import { KitchenSinkAppComponent } from "./Components/kitchen-sink-app/kitchen-sink-app.component";
 import { CometChatConversationListPageComponent } from "./Components/Pages/comet-chat-conversation-list-page/comet-chat-conversation-list-page.component";
+import { CometChatConversationListScreenPageComponent } from "./Components/Pages/comet-chat-conversation-list-screen-page/comet-chat-conversation-list-screen-page.component";
 import { CometChatMessageComposerPageComponent } from "./Components/Pages/comet-chat-message-composer-page/comet-chat-message-composer-page.component";
 import { UserListPageComponent } from "./Components/user-list-page/user-list-page.component";
 import { UserListScreenPageComponent } from "./Components/user-list-screen-page/user-list-screen-page.component";
@@ -32,6 +33,11 @@ const routes: Routes = [
   {
     path: "contact-screen",
     component: UserListScreenPageComponent,
+  },
+  ,
+  {
+    path: "/conversation-screen",
+    component: CometChatConversationListScreenPageComponent,
   },
 ];
 
