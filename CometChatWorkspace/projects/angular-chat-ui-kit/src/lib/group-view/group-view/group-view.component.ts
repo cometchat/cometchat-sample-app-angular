df3bbe5c763ace6f14af0be8fd4292d18e796487
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-11 17:40:43 +0530

    group Data Integration and group clicked functionality

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/group-view/group-view/group-view.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/group-view/group-view/group-view.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'group-view',
-  templateUrl: './group-view.component.html',
-  styleUrls: ['./group-view.component.css']
+  selector: "group-view",
+  templateUrl: "./group-view.component.html",
+  styleUrls: ["./group-view.component.css"],
 })
 export class GroupViewComponent implements OnInit {
+  @Input() group = null;
+  @Input() selectedGroup = null;
 
-  constructor() { }
+  @Output() onGroupClick: EventEmitter<any> = new EventEmitter();
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  /**
+   * Emitting the Group clicked so that it can be used in the parent component
+   * @param Any group
+   */
+  groupClicked(group) {
+    console.log("group view --> group clicked is ", group);
+    this.onGroupClick.emit(group);
   }
-
 }
