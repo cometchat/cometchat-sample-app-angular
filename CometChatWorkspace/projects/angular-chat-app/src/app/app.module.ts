c63478cf2956284f51cadad807926fee76113bd4
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-11 13:47:14 +0530

    cometChatConversationListScreenPage added

--- CometChatWorkspace/projects/angular-chat-app/src/app/app.module.ts
+++ CometChatWorkspace/projects/angular-chat-app/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { UserListPageComponent } from "./Components/user-list-page/user-list-pag
 import { CometChatConversationListPageComponent } from "./Components/Pages/comet-chat-conversation-list-page/comet-chat-conversation-list-page.component";
 import { UserListScreenPageComponent } from "./Components/user-list-screen-page/user-list-screen-page.component";
 import { CometChatMessageComposerPageComponent } from "./Components/Pages/comet-chat-message-composer-page/comet-chat-message-composer-page.component";
+import { CometChatConversationListScreenPageComponent } from './Components/Pages/comet-chat-conversation-list-screen-page/comet-chat-conversation-list-screen-page.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { CometChatMessageComposerPageComponent } from "./Components/Pages/comet-
     CometChatConversationListPageComponent,
     UserListScreenPageComponent,
     CometChatMessageComposerPageComponent,
+    CometChatConversationListScreenPageComponent,
   ],
   imports: [
     BrowserModule,
