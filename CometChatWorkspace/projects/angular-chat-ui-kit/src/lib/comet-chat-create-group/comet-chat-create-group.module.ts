49b4c87c007ec5bc52545cd1a8082205f8c71663
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-11 14:50:21 +0530

    backdrop module imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-create-group/comet-chat-create-group.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-create-group/comet-chat-create-group.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometChatCreateGroupComponent } from "./comet-chat-create-group/comet-chat-create-group.component";
+import { BackdropModule } from "../backdrop/backdrop.module";
 
 @NgModule({
   declarations: [CometChatCreateGroupComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, BackdropModule],
   exports: [CometChatCreateGroupComponent],
 })
 export class CometChatCreateGroupModule {}
