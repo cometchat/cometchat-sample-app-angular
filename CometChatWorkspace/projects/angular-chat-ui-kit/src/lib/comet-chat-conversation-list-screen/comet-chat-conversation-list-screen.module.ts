97bb5a01c94667dfcea4d72f70d177849f0efeaf
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-11 13:27:38 +0530

    comet-chat-conversation-list-screen created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-conversation-list-screen/comet-chat-conversation-list-screen.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CometChatConversationListScreenComponent } from './comet-chat-conversation-list-screen/comet-chat-conversation-list-screen.component';
+
+
+
+@NgModule({
+  declarations: [CometChatConversationListScreenComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CometChatConversationListScreenModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-conversation-list-screen/comet-chat-conversation-list-screen/comet-chat-conversation-list-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'comet-chat-conversation-list-screen',
+  templateUrl: './comet-chat-conversation-list-screen.component.html',
+  styleUrls: ['./comet-chat-conversation-list-screen.component.css']
+})
+export class CometChatConversationListScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
