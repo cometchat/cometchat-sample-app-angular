92f00491aeb8eda774d0f8cfc02fdc10ab3fcc2e
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-14 12:31:10 +0530

    live-reaction module created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/live-reaction/live-reaction.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LiveReactionComponent } from './live-reaction/live-reaction.component';
+
+
+
+@NgModule({
+  declarations: [LiveReactionComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LiveReactionModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/live-reaction/live-reaction/live-reaction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'live-reaction',
+  templateUrl: './live-reaction.component.html',
+  styleUrls: ['./live-reaction.component.css']
+})
+export class LiveReactionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
