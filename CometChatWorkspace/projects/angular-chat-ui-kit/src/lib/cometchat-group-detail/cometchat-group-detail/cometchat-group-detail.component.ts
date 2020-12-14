7c15f2fd8afe47db4ad3e9a0d4a683d1067aa447
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-14 17:58:01 +0530

    group detail component created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-group-detail/cometchat-group-detail/cometchat-group-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cometchat-group-detail',
+  templateUrl: './cometchat-group-detail.component.html',
+  styleUrls: ['./cometchat-group-detail.component.css']
+})
+export class CometchatGroupDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
