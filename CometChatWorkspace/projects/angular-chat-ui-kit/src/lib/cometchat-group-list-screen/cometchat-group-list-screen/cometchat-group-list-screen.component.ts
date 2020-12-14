f89157ebce46f218f95ea1aa12d0c4816a1f9f08
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-14 14:57:00 +0530

    groupListScreen component created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-group-list-screen/cometchat-group-list-screen/cometchat-group-list-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cometchat-group-list-screen',
+  templateUrl: './cometchat-group-list-screen.component.html',
+  styleUrls: ['./cometchat-group-list-screen.component.css']
+})
+export class CometchatGroupListScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
