a6dee369a8df2390d60d11200e4904d312a76199
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-14 17:47:06 +0530

    Message List Screen , Message thread , group list, ImageView modules imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-group-list-screen/cometchat-group-list-screen.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-group-list-screen/cometchat-group-list-screen.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometchatGroupListScreenComponent } from "./cometchat-group-list-screen/cometchat-group-list-screen.component";
+import { CometChatGroupListModule } from "../comet-chat-group-list/comet-chat-group-list.module";
+import { CometchatMessageListScreenModule } from "../cometchat-message-list-screen/cometchat-message-list-screen.module";
+import { MessageThreadModule } from "../message-thread/message-thread.module";
+import { CometChatUserDetailModule } from "../comet-chat-user-detail/comet-chat-user-detail.module";
+import { ImageViewModule } from "../image-view/image-view.module";
 
 @NgModule({
   declarations: [CometchatGroupListScreenComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CometChatGroupListModule,
+    CometchatMessageListScreenModule,
+    MessageThreadModule,
+    CometChatUserDetailModule,
+    ImageViewModule,
+  ],
   exports: [CometchatGroupListScreenComponent],
 })
 export class CometchatGroupListScreenModule {}
