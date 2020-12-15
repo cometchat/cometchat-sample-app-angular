2dcc52dfb07dbb975d153ccc778ebfd2c6790d6d
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-15 16:35:41 +0530

    created function to display stickers

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-sticker-bubble/sender-sticker-bubble/sender-sticker-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-sticker-bubble/sender-sticker-bubble/sender-sticker-bubble.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  Output,
+  EventEmitter,
+  SimpleChanges,
+  OnChanges,
+} from "@angular/core";
 
 @Component({
-  selector: 'sender-sticker-bubble',
-  templateUrl: './sender-sticker-bubble.component.html',
-  styleUrls: ['./sender-sticker-bubble.component.css']
+  selector: "sender-sticker-bubble",
+  templateUrl: "./sender-sticker-bubble.component.html",
+  styleUrls: ["./sender-sticker-bubble.component.css"],
 })
-export class SenderStickerBubbleComponent implements OnInit {
+export class SenderStickerBubbleComponent implements OnInit, OnChanges {
+  @Input() MessageDetails = null;
+  @Input() showToolTip = true;
 
-  constructor() { }
+  @Input() showReplyCount = true;
+
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
+
+  messageFrom = "sender";
+  message;
+  stickerUrl: string;
+  constructor() {}
+
+  ngOnChanges(change: SimpleChanges) {}
 
   ngOnInit() {
+    let stickerData = null;
+    let stickerImg = null;
+    if (
+      this.MessageDetails.hasOwnProperty("data") &&
+      this.MessageDetails.data.hasOwnProperty("customData")
+    ) {
+      stickerData = this.MessageDetails.data.customData;
+      if (stickerData.hasOwnProperty("sticker_url")) {
+        const stickerName = stickerData.hasOwnProperty("sticker_name")
+          ? stickerData.sticker_name
+          : "Sticker";
+        this.stickerUrl = stickerData.sticker_url;
+      }
+    }
   }
-
+  actionHandler(action) {
+    this.actionGenerated.emit(action);
+  }
 }
