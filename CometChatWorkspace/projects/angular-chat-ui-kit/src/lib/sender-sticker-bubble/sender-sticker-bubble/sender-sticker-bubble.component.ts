5acb074cd6b1af7d983c3524aec49aac7320db37
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-15 18:11:45 +0530

    added ngOnChanges

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-sticker-bubble/sender-sticker-bubble/sender-sticker-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-sticker-bubble/sender-sticker-bubble/sender-sticker-bubble.component.ts
@@ -24,20 +24,32 @@ export class SenderStickerBubbleComponent implements OnInit, OnChanges {
   messageFrom = "sender";
   message;
   stickerUrl: string;
+  stickerName: string;
   constructor() {}
 
-  ngOnChanges(change: SimpleChanges) {}
+  ngOnChanges(change: SimpleChanges) {
+    if (change["MessageDetails"]) {
+      if (
+        change["MessageDetails"].previousValue !==
+        change["MessageDetails"].currentValue
+      ) {
+        const message = Object.assign({}, this.MessageDetails, {
+          messageFrom: this.messageFrom,
+        });
+        this.MessageDetails = message;
+      }
+    }
+  }
 
   ngOnInit() {
     let stickerData = null;
-    let stickerImg = null;
     if (
       this.MessageDetails.hasOwnProperty("data") &&
       this.MessageDetails.data.hasOwnProperty("customData")
     ) {
       stickerData = this.MessageDetails.data.customData;
       if (stickerData.hasOwnProperty("sticker_url")) {
-        const stickerName = stickerData.hasOwnProperty("sticker_name")
+        this.stickerName = stickerData.hasOwnProperty("sticker_name")
           ? stickerData.sticker_name
           : "Sticker";
         this.stickerUrl = stickerData.sticker_url;
