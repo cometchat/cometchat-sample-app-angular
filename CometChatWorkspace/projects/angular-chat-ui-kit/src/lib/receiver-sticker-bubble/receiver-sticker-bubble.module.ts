3685b471e847108f0732de009c86d55153cbfee9
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-15 16:42:00 +0530

    created receiver-sticker-bubble module

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-sticker-bubble/receiver-sticker-bubble.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReceiverStickerBubbleComponent } from './receiver-sticker-bubble/receiver-sticker-bubble.component';
+
+
+
+@NgModule({
+  declarations: [ReceiverStickerBubbleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReceiverStickerBubbleModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-sticker-bubble/receiver-sticker-bubble/receiver-sticker-bubble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'receiver-sticker-bubble',
+  templateUrl: './receiver-sticker-bubble.component.html',
+  styleUrls: ['./receiver-sticker-bubble.component.css']
+})
+export class ReceiverStickerBubbleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
