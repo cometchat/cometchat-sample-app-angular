7c6b74fd1bed15243414bd7bd167d926c298dcdf
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-15 14:02:01 +0530

    sticker-view component exported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sticker-view/sticker-view.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sticker-view/sticker-view.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { StickerViewComponent } from './sticker-view/sticker-view.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { StickerViewComponent } from "./sticker-view/sticker-view.component";
 
 @NgModule({
   declarations: [StickerViewComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [StickerViewComponent],
 })
-export class StickerViewModule { }
+export class StickerViewModule {}
