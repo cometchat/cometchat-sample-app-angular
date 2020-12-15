43222b04af7c1fdee423c703d8598f3c94de931f
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-15 17:14:39 +0530

    receiver-sticker-bubble exported and reply-count,avatar,tooltip imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-sticker-bubble/receiver-sticker-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-sticker-bubble/receiver-sticker-bubble.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReceiverStickerBubbleComponent } from './receiver-sticker-bubble/receiver-sticker-bubble.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReceiverStickerBubbleComponent } from "./receiver-sticker-bubble/receiver-sticker-bubble.component";
+import { ToolTipModule } from "../tool-tip/tool-tip.module";
+import { ReplyCountModule } from "../reply-count/reply-count.module";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [ReceiverStickerBubbleComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ToolTipModule,
+    ReplyCountModule,
+    AvatarModule,
+    ReplyCountModule,
+  ],
+  exports: [ReceiverStickerBubbleComponent],
 })
-export class ReceiverStickerBubbleModule { }
+export class ReceiverStickerBubbleModule {}
