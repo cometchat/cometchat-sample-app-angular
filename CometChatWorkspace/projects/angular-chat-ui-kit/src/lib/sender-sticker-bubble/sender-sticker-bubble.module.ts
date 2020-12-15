030784bf65ce78e545a5d02028e4076ea152b7ed
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-15 14:52:21 +0530

    sender-sticker-bubble created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-sticker-bubble/sender-sticker-bubble.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SenderStickerBubbleComponent } from './sender-sticker-bubble/sender-sticker-bubble.component';
+
+
+
+@NgModule({
+  declarations: [SenderStickerBubbleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SenderStickerBubbleModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/sender-sticker-bubble/sender-sticker-bubble/sender-sticker-bubble.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sender-sticker-bubble',
+  templateUrl: './sender-sticker-bubble.component.html',
+  styleUrls: ['./sender-sticker-bubble.component.css']
+})
+export class SenderStickerBubbleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
