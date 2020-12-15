a5d6d08ca1e57180215daf1bb0a7b618ba27bbf1
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-15 17:15:56 +0530

    created function to display sticker

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-sticker-bubble/receiver-sticker-bubble/receiver-sticker-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-sticker-bubble/receiver-sticker-bubble/receiver-sticker-bubble.component.ts
@@ -1,15 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'receiver-sticker-bubble',
-  templateUrl: './receiver-sticker-bubble.component.html',
-  styleUrls: ['./receiver-sticker-bubble.component.css']
+  selector: "receiver-sticker-bubble",
+  templateUrl: "./receiver-sticker-bubble.component.html",
+  styleUrls: ["./receiver-sticker-bubble.component.css"],
 })
 export class ReceiverStickerBubbleComponent implements OnInit {
+  @Input() MessageDetails = null;
+  @Input() showToolTip = true;
 
-  constructor() { }
+  @Input() showReplyCount = true;
+
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
+
+  avatar = null;
+  //Sets Username of Avatar
+  name: string = null;
+  //If Group then only show avatar
+  //If Group then only show avatar
+  avatarIfGroup: boolean = false;
+
+  stickerName: string;
+  stickerUrl: string;
+  constructor() {}
 
   ngOnInit() {
+    /**
+     *  If Group then displays Avatar And Name
+     */
+    if (this.MessageDetails.receiverType === "group") {
+      this.avatarIfGroup = true;
+
+      if (!this.MessageDetails.sender.avatar) {
+        const uid = this.MessageDetails.sender.getUid();
+        const char = this.MessageDetails.sender
+          .getName()
+          .charAt(0)
+          .toUpperCase();
+        // this.MessageDetails.sender.setAvatar(SvgAvatar.getAvatar(uid, char));
+      }
+      this.name = this.MessageDetails.sender.name;
+      this.avatar = this.MessageDetails.sender.avatar;
+    }
+    this.getSticker();
   }
 
+  /**
+   * Get Sticker Details
+   */
+  getSticker() {
+    let stickerData = null;
+    if (
+      this.MessageDetails.hasOwnProperty("data") &&
+      this.MessageDetails.data.hasOwnProperty("customData")
+    ) {
+      stickerData = this.MessageDetails.data.customData;
+      if (stickerData.hasOwnProperty("sticker_url")) {
+        this.stickerName = stickerData.hasOwnProperty("sticker_name")
+          ? stickerData.sticker_name
+          : "Sticker";
+        this.stickerUrl = stickerData.sticker_url;
+      }
+    }
+  }
+
+  /**
+   * Set Time-Stamp for receiving sticker
+   *
+   */
+  getTime() {
+    let msgSentAt = this.MessageDetails.sentAt;
+    let timeStamp = new Date(msgSentAt * 1000).toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+    return timeStamp;
+  }
+
+  /**
+   * Handles all the actions emitted by the child components that make the current component
+   * @param Event action
+   */
+  actionHandler(action) {
+    console.log("receiver Sticker Bubble --> action generation is ", action);
+    this.actionGenerated.emit(action);
+  }
 }
