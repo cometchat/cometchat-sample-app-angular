cdfca74b9aa0bf64c99845d556e9a9cc0f3587b7
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-16 17:35:55 +0530

    function created to check if link-preview extension is enabled

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-sender-message-bubble/comet-chat-sender-message-bubble/comet-chat-sender-message-bubble.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-sender-message-bubble/comet-chat-sender-message-bubble/comet-chat-sender-message-bubble.component.ts
@@ -13,9 +13,60 @@ export class CometChatSenderMessageBubbleComponent implements OnInit {
 
   @Input() showToolTip = true;
 
+  linkPreview: boolean = false;
+  linkTitle: string;
+  linkDescription: string;
+  linkUrl: string;
+  linkText: string;
+  linkImage: string;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.checkLinkPreview();
+  }
+
+  /**
+   * Check If extension has enabled LinkPreview
+   */
+  checkLinkPreview() {
+    if (this.MessageDetails.hasOwnProperty("metadata")) {
+      const metadata = this.MessageDetails.metadata;
+      const injectedObject = metadata["@injected"];
+      if (injectedObject && injectedObject.hasOwnProperty("extensions")) {
+        const extensionsObject = injectedObject["extensions"];
+        if (
+          extensionsObject &&
+          extensionsObject.hasOwnProperty("link-preview")
+        ) {
+          const linkPreviewObject = extensionsObject["link-preview"];
+          if (
+            linkPreviewObject &&
+            linkPreviewObject.hasOwnProperty("links") &&
+            linkPreviewObject["links"].length
+          ) {
+            this.linkPreview = true;
+            const linkObject = linkPreviewObject["links"][0];
+            this.linkTitle = linkObject.title;
+            this.linkDescription = linkObject.description;
+            this.linkUrl = linkObject.url;
+            this.linkImage = linkObject.image;
+            console.log("sender link obj ", linkObject);
+            console.log(this.linkTitle !== "");
+
+            console.log("sender link url ", this.linkUrl);
+
+            const pattern = /(http:|https:)?\/\/(www\.)?(youtube.com|youtu.be)(\S+)?/;
+            const linkText = linkObject["url"].match(pattern)
+              ? "View on Youtube"
+              : "Visit";
+            this.linkText = linkText;
+            // const actualMessage = messageText;
+          }
+        }
+      }
+    }
+  }
 
   /**
    * Handles all the actions emitted by the child components that make the current component
