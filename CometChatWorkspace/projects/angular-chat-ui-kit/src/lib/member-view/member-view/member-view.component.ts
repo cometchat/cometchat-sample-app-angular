0ba5b4c11600f941cbc4a9f4d864279fe0334609
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-16 15:51:36 +0530

    ban , kick , change scope of member action added

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/member-view/member-view/member-view.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/member-view/member-view/member-view.component.ts
@@ -1,15 +1,98 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
+import { CometChat } from "@cometchat-pro/chat";
 @Component({
-  selector: 'member-view',
-  templateUrl: './member-view.component.html',
-  styleUrls: ['./member-view.component.css']
+  selector: "member-view",
+  templateUrl: "./member-view.component.html",
+  styleUrls: ["./member-view.component.css"],
 })
 export class MemberViewComponent implements OnInit {
+  @Input() item = null;
+  @Input() type = null;
+  @Input() member = null;
 
-  constructor() { }
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
+
+  scope;
+  showChangeScope: boolean = false;
+  roles = {};
+  roleCodes = [];
+  PARTICIPANT = CometChat.GROUP_MEMBER_SCOPE.PARTICIPANT;
+
+  constructor() {}
 
   ngOnInit() {
+    // console.log("member view --> member details ", this.member);
+    this.scope = this.member.scope;
+
+    this.setRoles();
   }
 
+  /**
+   * Sets the values for the roles dropdown
+   * @param
+   */
+  setRoles() {
+    this.roles[CometChat.GROUP_MEMBER_SCOPE.ADMIN] = "Administrator";
+    this.roles[CometChat.GROUP_MEMBER_SCOPE.MODERATOR] = "Moderator";
+    this.roles[CometChat.GROUP_MEMBER_SCOPE.PARTICIPANT] = "Participant";
+
+    this.roleCodes = [
+      CometChat.GROUP_MEMBER_SCOPE.ADMIN,
+      CometChat.GROUP_MEMBER_SCOPE.MODERATOR,
+      CometChat.GROUP_MEMBER_SCOPE.PARTICIPANT,
+    ];
+
+    //console.log("member view --> roles ", this.roles);
+  }
+
+  /**
+   * Closes or opens  the edit scope dropdown field
+   * @param
+   */
+  toggleChangeScope(show) {
+    this.showChangeScope = show;
+  }
+
+  /**
+   * Closes or opens  the edit scope dropdown field
+   * @param Event event
+   */
+  scopeChangeHandler(event) {
+    //console.log("member view --> scope changed", event.target.value);
+    this.scope = event.target.value;
+  }
+
+  /**
+   * emits an event to update the scope of the current member
+   * @param
+   */
+  updateMemberScope = () => {
+    this.actionGenerated.emit({
+      type: "changescope",
+      payLoad: { member: this.member, scope: this.scope },
+    });
+    this.toggleChangeScope(false);
+  };
+
+  /**
+   * emits an event to ban  the current member
+   * @param
+   */
+  banMember = () => {
+    this.actionGenerated.emit({
+      type: "ban",
+      payLoad: { member: this.member, scope: this.scope },
+    });
+  };
+
+  /**
+   * emits an event to kick the current member out of the group
+   * @param
+   */
+  kickMember = () => {
+    this.actionGenerated.emit({
+      type: "kick",
+      payLoad: { member: this.member, scope: this.scope },
+    });
+  };
 }
