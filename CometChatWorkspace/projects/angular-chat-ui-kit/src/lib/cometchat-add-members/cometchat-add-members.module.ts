5223c33b5da8c9e0e1755f6c71f7151a80c8caba
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-16 19:59:21 +0530

    backdrop module imported and integrated

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-add-members/cometchat-add-members.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-add-members/cometchat-add-members.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometchatAddMembersComponent } from "./cometchat-add-members/cometchat-add-members.component";
+import { BackdropModule } from "../backdrop/backdrop.module";
 
 @NgModule({
   declarations: [CometchatAddMembersComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, BackdropModule],
   exports: [CometchatAddMembersComponent],
 })
 export class CometchatAddMembersModule {}
