9c3c63bdaf5a28e757f7b56bea5a41d1c299e468
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-16 15:54:44 +0530

    cometChatViewMembers and shared media  module imported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-group-detail/cometchat-group-detail.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-group-detail/cometchat-group-detail.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometchatGroupDetailComponent } from "./cometchat-group-detail/cometchat-group-detail.component";
+import { SharedMediaViewModule } from "../shared-media-view/shared-media-view.module";
+import { CometchatViewMembersModule } from "../cometchat-view-members/cometchat-view-members.module";
 
 @NgModule({
   declarations: [CometchatGroupDetailComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedMediaViewModule, CometchatViewMembersModule],
   exports: [CometchatGroupDetailComponent],
 })
 export class CometchatGroupDetailModule {}
