a0ebd8f6681a1721f18192ade23e81ddabf62099
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-17 18:54:56 +0530

    user data integration and select functionality

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/add-member-view/add-member-view/add-member-view.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/add-member-view/add-member-view/add-member-view.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'add-member-view',
-  templateUrl: './add-member-view.component.html',
-  styleUrls: ['./add-member-view.component.css']
+  selector: "add-member-view",
+  templateUrl: "./add-member-view.component.html",
+  styleUrls: ["./add-member-view.component.css"],
 })
 export class AddMemberViewComponent implements OnInit {
+  @Input() item = null;
+  @Input() type = null;
+  @Input() user = null;
+  @Input() members = null;
 
-  constructor() { }
+  checked: boolean = false;
+
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
 
   ngOnInit() {
+    console.log("add member view --> user ", this.user);
+
+    this.checked = this.members.find((member) => member.uid === this.user.uid);
   }
 
+  /**
+   * toggle the checkbox for each users , that is, to add them or not to add them in the group
+   * @param Event event
+   */
+  handleCheck(event) {
+    this.checked = !this.checked;
+
+    console.log(`user is checked`, this.checked);
+
+    this.actionGenerated.emit({
+      type: "memberUpdated",
+      payLoad: { user: this.user, userState: this.checked },
+    });
+
+    // props.changed(props.user, value);
+  }
 }
