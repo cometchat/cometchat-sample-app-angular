796850ca2f5d29222eaad40ec89f979506e56436
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-17 18:15:34 +0530

    imported comet-chat-ban-members

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-group-detail/cometchat-group-detail.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/cometchat-group-detail/cometchat-group-detail.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from "@angular/common";
 import { CometchatGroupDetailComponent } from "./cometchat-group-detail/cometchat-group-detail.component";
 import { SharedMediaViewModule } from "../shared-media-view/shared-media-view.module";
 import { CometchatViewMembersModule } from "../cometchat-view-members/cometchat-view-members.module";
+import { CometChatBanMembersModule } from "../comet-chat-ban-members/comet-chat-ban-members.module";
 
 @NgModule({
   declarations: [CometchatGroupDetailComponent],
-  imports: [CommonModule, SharedMediaViewModule, CometchatViewMembersModule],
+  imports: [
+    CommonModule,
+    SharedMediaViewModule,
+    CometchatViewMembersModule,
+    CometChatBanMembersModule,
+  ],
   exports: [CometchatGroupDetailComponent],
 })
 export class CometchatGroupDetailModule {}
