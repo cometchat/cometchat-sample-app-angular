20e565bb33d8ba1174ed987ab86b7901c31aea66
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-17 21:01:05 +0530

    exported cometChatBanMemberComponent and imported backdrop and banmemeber

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-ban-members/comet-chat-ban-members.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-ban-members/comet-chat-ban-members.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CometChatBanMembersComponent } from './comet-chat-ban-members/comet-chat-ban-members.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CometChatBanMembersComponent } from "./comet-chat-ban-members/comet-chat-ban-members.component";
+import { BackdropModule } from "../backdrop/backdrop.module";
+import { BanMemberViewModule } from "../ban-member-view/ban-member-view.module";
 
 @NgModule({
   declarations: [CometChatBanMembersComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, BackdropModule, BanMemberViewModule],
+  exports: [CometChatBanMembersComponent],
 })
-export class CometChatBanMembersModule { }
+export class CometChatBanMembersModule {}
