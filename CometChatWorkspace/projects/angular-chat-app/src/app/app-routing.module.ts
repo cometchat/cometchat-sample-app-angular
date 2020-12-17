9584122c9a0c3f13bb3412a30d17b350c622303b
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-17 17:38:48 +0530

    routed to grop-list-screen-page-component

--- CometChatWorkspace/projects/angular-chat-app/src/app/app-routing.module.ts
+++ CometChatWorkspace/projects/angular-chat-app/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomePageComponent } from "./Components/home-page/home-page.component";
 import { KitchenSinkAppComponent } from "./Components/kitchen-sink-app/kitchen-sink-app.component";
 import { CometChatConversationListPageComponent } from "./Components/Pages/comet-chat-conversation-list-page/comet-chat-conversation-list-page.component";
+import { CometChatGroupListScreenPageComponent } from "./Components/Pages/comet-chat-group-list-screen-page/comet-chat-group-list-screen-page.component";
 import { CometChatMessageComposerPageComponent } from "./Components/Pages/comet-chat-message-composer-page/comet-chat-message-composer-page.component";
 import { UserListPageComponent } from "./Components/user-list-page/user-list-page.component";
 import { UserListScreenPageComponent } from "./Components/user-list-screen-page/user-list-screen-page.component";
@@ -32,6 +33,10 @@ const routes: Routes = [
   {
     path: "contact-screen",
     component: UserListScreenPageComponent,
+  },
+  {
+    path: "group-screen",
+    component: CometChatGroupListScreenPageComponent,
   },
 ];
 
