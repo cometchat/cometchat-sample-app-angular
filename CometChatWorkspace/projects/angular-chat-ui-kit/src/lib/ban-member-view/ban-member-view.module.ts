87db86c5ede66eb162363c1502f3a571ebbbead7
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-17 20:58:35 +0530

    exported ban-member-view component

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/ban-member-view/ban-member-view.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/ban-member-view/ban-member-view.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BanMemberViewComponent } from './ban-member-view/ban-member-view.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BanMemberViewComponent } from "./ban-member-view/ban-member-view.component";
 
 @NgModule({
   declarations: [BanMemberViewComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [BanMemberViewComponent],
 })
-export class BanMemberViewModule { }
+export class BanMemberViewModule {}
