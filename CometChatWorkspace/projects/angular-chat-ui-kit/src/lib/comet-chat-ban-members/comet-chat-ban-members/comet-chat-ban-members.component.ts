72c46fc42b704bcde2e647605108c32f0d50df45
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-17 21:03:39 +0530

    created function to scroll,unban member and emit action to close banmember window

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-ban-members/comet-chat-ban-members/comet-chat-ban-members.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-ban-members/comet-chat-ban-members/comet-chat-ban-members.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
+import { CometChat } from "@cometchat-pro/chat";
 
 @Component({
-  selector: 'comet-chat-ban-members',
-  templateUrl: './comet-chat-ban-members.component.html',
-  styleUrls: ['./comet-chat-ban-members.component.css']
+  selector: "comet-chat-ban-members",
+  templateUrl: "./comet-chat-ban-members.component.html",
+  styleUrls: ["./comet-chat-ban-members.component.css"],
 })
 export class CometChatBanMembersComponent implements OnInit {
+  @Input() item = null;
+  @Input() type = null;
+  @Input() bannedmemberlist = [];
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  decoratorMessage = "Loading...";
+  membersToBan = [];
+  membersToUnban = [];
+
+  constructor() {}
 
   ngOnInit() {
+    console.log("memberList", this.bannedmemberlist);
   }
 
+  unbanMember(memberToUnBan) {
+    // const group = this.context;
+    console.log("member to unban ", memberToUnBan);
+
+    const guid = this.item.guid;
+    CometChat.unbanGroupMember(guid, memberToUnBan.uid)
+      .then((response) => {
+        if (response) {
+          console.log("Group member unbanning success with response", response);
+          this.actionGenerated.emit({
+            type: "unbanGroupMembers",
+            payLoad: [memberToUnBan],
+          });
+        }
+      })
+      .catch((error) => {
+        console.log("Group member banning failed with error", error);
+      });
+  }
+
+  actionHandler(action) {
+    let data = action.payLoad;
+
+    switch (action.type) {
+      case "unban":
+        this.unbanMember(data.member);
+        break;
+      default:
+        break;
+    }
+  }
+  handleScroll(e) {
+    const bottom =
+      Math.round(e.currentTarget.scrollHeight - e.currentTarget.scrollTop) ===
+      Math.round(e.currentTarget.clientHeight);
+    if (bottom) {
+      this.actionGenerated.emit({
+        type: "fetchBannedMembers",
+      });
+    }
+  }
+
+  closeBanMemberModal() {
+    console.log("cometchat ban member --> close ban member clicked");
+
+    this.actionGenerated.emit({ type: "banmember", payLoad: null });
+  }
 }
