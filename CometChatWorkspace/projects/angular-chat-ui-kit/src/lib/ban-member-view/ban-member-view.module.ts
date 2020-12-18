d99d37577f7e974494a4ff013e8dbb126bbc657b
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-18 12:44:22 +0530

    imported avatar module

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/ban-member-view/ban-member-view.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/ban-member-view/ban-member-view.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { BanMemberViewComponent } from "./ban-member-view/ban-member-view.component";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [BanMemberViewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AvatarModule],
   exports: [BanMemberViewComponent],
 })
 export class BanMemberViewModule {}
