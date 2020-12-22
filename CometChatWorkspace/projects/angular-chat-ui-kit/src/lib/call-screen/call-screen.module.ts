dae95af8f080200b95d04221558e74e8994464b0
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-22 20:37:50 +0530

    avatar module imported in callScreenComponent

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/call-screen/call-screen.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/call-screen/call-screen.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CallScreenComponent } from "./call-screen/call-screen.component";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [CallScreenComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AvatarModule],
   exports: [CallScreenComponent],
 })
 export class CallScreenModule {}
