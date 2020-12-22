7325d2770fcccdf71d16daaf53ecce4e1dc9ec73
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-22 11:54:29 +0530

    created call-alert module

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/call-alert/call-alert.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CallAlertComponent } from './call-alert/call-alert.component';
+
+
+
+@NgModule({
+  declarations: [CallAlertComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CallAlertModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/call-alert/call-alert/call-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'call-alert',
+  templateUrl: './call-alert.component.html',
+  styleUrls: ['./call-alert.component.css']
+})
+export class CallAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
