9b413fa6a48c24f6cecb8f1299e39c17544c74c7
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-22 15:31:06 +0530

    imported avatar module and exported callAlert Component

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/call-alert/call-alert.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/call-alert/call-alert.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CallAlertComponent } from './call-alert/call-alert.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CallAlertComponent } from "./call-alert/call-alert.component";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [CallAlertComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, AvatarModule],
+  exports: [CallAlertComponent],
 })
-export class CallAlertModule { }
+export class CallAlertModule {}
