69e06bbf776a0e6de5f999f92a4e27fbab9a2bc1
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-23 15:48:36 +0530

    call-message component exported

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/call-message/call-message.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/call-message/call-message.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CallMessageComponent } from './call-message/call-message.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CallMessageComponent } from "./call-message/call-message.component";
 
 @NgModule({
   declarations: [CallMessageComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [CallMessageComponent],
 })
-export class CallMessageModule { }
+export class CallMessageModule {}
