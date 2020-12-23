50a46d73c3dd78548a3aecb9289ee601a2dbe1bd
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-23 17:29:25 +0530

    avatar module imported in receiver poll bubble

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/receiver-poll-bubble/receiver-poll-bubble.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReceiverPollBubbleComponent } from "./receiver-poll-bubble/receiver-poll-bubble.component";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [ReceiverPollBubbleComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AvatarModule],
   exports: [ReceiverPollBubbleComponent],
 })
 export class ReceiverPollBubbleModule {}
