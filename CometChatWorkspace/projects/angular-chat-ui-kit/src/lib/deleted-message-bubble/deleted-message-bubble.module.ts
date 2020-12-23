0af90f68fae9a3a3816fa40d9115b29b7fd3376b
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-23 18:48:42 +0530

    avatar module imported and integrated in delete message bubble

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/deleted-message-bubble/deleted-message-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/deleted-message-bubble/deleted-message-bubble.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DeletedMessageBubbleComponent } from "./deleted-message-bubble/deleted-message-bubble.component";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [DeletedMessageBubbleComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AvatarModule],
   exports: [DeletedMessageBubbleComponent],
 })
 export class DeletedMessageBubbleModule {}
