41e801a8b20fa4864290b395fcd5236c82ca778e
Sohail khan <sohailkhan@Sohail-Khan-B366s-MacBook-Pro.local>
2020-12-23 16:51:29 +0530

    avatar module imported in messageReceiverBubble

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-receiver-message-bubble/comet-chat-receiver-message-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-receiver-message-bubble/comet-chat-receiver-message-bubble.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { CometChatReceiverMessageBubbleComponent } from "./comet-chat-receiver-message-bubble/comet-chat-receiver-message-bubble.component";
 import { ToolTipModule } from "../tool-tip/tool-tip.module";
 import { ReplyCountModule } from "../reply-count/reply-count.module";
+import { AvatarModule } from "../avatar/avatar.module";
 
 @NgModule({
   declarations: [CometChatReceiverMessageBubbleComponent],
-  imports: [CommonModule, ToolTipModule, ReplyCountModule],
+  imports: [CommonModule, ToolTipModule, ReplyCountModule, AvatarModule],
   exports: [CometChatReceiverMessageBubbleComponent],
 })
 export class CometChatReceiverMessageBubbleModule {}
