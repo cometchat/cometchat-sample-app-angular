75783e524bdb9c3a32282dc01d45cd13cabc518b
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-28 11:30:28 +0530

    cometChatUserInfo module created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-user-info-screen/comet-chat-user-info-screen.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CometChatUserInfoScreenComponent } from './comet-chat-user-info-screen/comet-chat-user-info-screen.component';
+
+
+
+@NgModule({
+  declarations: [CometChatUserInfoScreenComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CometChatUserInfoScreenModule { }
---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-user-info-screen/comet-chat-user-info-screen/comet-chat-user-info-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'comet-chat-user-info-screen',
+  templateUrl: './comet-chat-user-info-screen.component.html',
+  styleUrls: ['./comet-chat-user-info-screen.component.css']
+})
+export class CometChatUserInfoScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
