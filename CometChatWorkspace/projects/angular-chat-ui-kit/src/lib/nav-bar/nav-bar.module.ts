da2e6d27acafbb9f27796ac4f7f6eff14ec68089
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-28 11:53:56 +0530

    nav-bar module created

---
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/nav-bar/nav-bar.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+
+
+@NgModule({
+  declarations: [NavBarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NavBarModule { }
