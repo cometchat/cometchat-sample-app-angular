86521ab78ed3b076a0bf9d90210746e24726a6ea
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-28 18:30:09 +0530

    created functions to open List and emit actions

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/nav-bar/nav-bar/nav-bar.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/nav-bar/nav-bar/nav-bar.component.ts
@@ -1,15 +1,87 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  selector: "nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styleUrls: ["./nav-bar.component.css"],
 })
 export class NavBarComponent implements OnInit {
+  @Output() actionGenerated: EventEmitter<any> = new EventEmitter();
+  @Output() onUserClick: EventEmitter<any> = new EventEmitter();
 
-  constructor() { }
+  displayConversationList: boolean = true;
+  displayGroupList: boolean = false;
+  displayUserList: boolean = false;
 
-  ngOnInit() {
+  displayUserInfoScreen: boolean = false;
+
+  item;
+  // curentItem;
+  lastMessage;
+  type;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  checkScreen(type) {
+    this.displayConversationList = type === "conversationList" ? true : false;
+    this.displayGroupList = type === "groupList" ? true : false;
+    this.displayUserList = type === "userList" ? true : false;
+    this.displayUserInfoScreen = type === "infoScreen" ? true : false;
   }
 
+  /**
+   *
+   */
+  openConversationList() {
+    // this.displayConversationList = true;
+    this.checkScreen("conversationList");
+    this.actionGenerated.emit({
+      type: "tabChanged",
+    });
+  }
+
+  openGroupList() {
+    this.checkScreen("groupList");
+
+    // this.displayGroupList = true;
+    this.actionGenerated.emit({
+      type: "tabChanged",
+    });
+  }
+  openUserList() {
+    this.checkScreen("userList");
+
+    // this.displayUserList = true;
+    this.actionGenerated.emit({
+      type: "tabChanged",
+    });
+  }
+  openUserInfoScreen() {
+    this.checkScreen("infoScreen");
+
+    // this.displayUserInfoScreen = true;
+    this.actionGenerated.emit({
+      type: "tabChanged",
+    });
+  }
+
+  userClicked(user) {
+    if (user.hasOwnProperty("conversationWith")) {
+      this.item = user.conversationWith;
+    } else {
+      this.item = user;
+    }
+    if (this.item.hasOwnProperty("uid")) {
+      this.type = "user";
+    } else {
+      this.type = "group";
+    }
+    this.lastMessage = user.lastMessage;
+
+    // this.curentItem = this.item;
+    console.log("navBar ", this.item);
+    this.onUserClick.emit(this.item);
+  }
 }
