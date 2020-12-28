062af457caa44ee20259619981e241d3ad8501bc
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-28 11:52:25 +0530

    created function to get info for loggedInUser

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-user-info-screen/comet-chat-user-info-screen/comet-chat-user-info-screen.component.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-user-info-screen/comet-chat-user-info-screen/comet-chat-user-info-screen.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { CometChat } from "@cometchat-pro/chat";
+import { CometChatManager } from "../../utils/controller";
 @Component({
-  selector: 'comet-chat-user-info-screen',
-  templateUrl: './comet-chat-user-info-screen.component.html',
-  styleUrls: ['./comet-chat-user-info-screen.component.css']
+  selector: "comet-chat-user-info-screen",
+  templateUrl: "./comet-chat-user-info-screen.component.html",
+  styleUrls: ["./comet-chat-user-info-screen.component.css"],
 })
 export class CometChatUserInfoScreenComponent implements OnInit {
-
-  constructor() { }
+  user;
+  constructor() {}
 
   ngOnInit() {
+    this.getProfile();
   }
 
+  getProfile() {
+    new CometChatManager()
+      .getLoggedInUser()
+      .then((user) => {
+        // this.setAvatar(user);
+        this.user = user;
+      })
+      .catch((error) => {
+        console.log(
+          "[CometChatUserInfoScreen] getProfile getLoggedInUser error",
+          error
+        );
+      });
+  }
 }
