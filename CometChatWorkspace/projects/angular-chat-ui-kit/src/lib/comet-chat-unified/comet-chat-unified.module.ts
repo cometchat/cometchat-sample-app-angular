3d902f13e02b129fea5af5fe907b0ec5e283ebab
Deepanshu Goyal <deepanshug@geekyants.com>
2020-12-28 18:33:32 +0530

    imported NavBar,CometChatMessageListScreenModule, CometChatUserDetail,MessageThreadModule,ImageView module

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-unified/comet-chat-unified.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/lib/comet-chat-unified/comet-chat-unified.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { CometChatUnifiedComponent } from "./comet-chat-unified/comet-chat-unified.component";
+import { NavBarModule } from "../nav-bar/nav-bar.module";
+import { CometchatMessageListScreenModule } from "../cometchat-message-list-screen/cometchat-message-list-screen.module";
+import { CometChatUserDetailModule } from "../comet-chat-user-detail/comet-chat-user-detail.module";
+import { MessageThreadModule } from "../message-thread/message-thread.module";
+import { ImageViewModule } from "../image-view/image-view.module";
 
 @NgModule({
   declarations: [CometChatUnifiedComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NavBarModule,
+    CometchatMessageListScreenModule,
+    CometChatUserDetailModule,
+    MessageThreadModule,
+    ImageViewModule,
+  ],
   exports: [CometChatUnifiedComponent],
 })
 export class CometChatUnifiedModule {}
