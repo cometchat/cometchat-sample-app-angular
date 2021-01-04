044cafa65415638368e09230cb551952dab89a9f
Deepanshu Goyal <deepanshug@geekyants.com>
2021-01-04 21:39:07 +0530

    exportd component and imported ngx module

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/components/cometchat-regular-reaction-view/cometchat-regular-reaction-view.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/components/cometchat-regular-reaction-view/cometchat-regular-reaction-view.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CometchatRegularReactionViewComponent } from './cometchat-regular-reaction-view/cometchat-regular-reaction-view.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CometchatRegularReactionViewComponent } from "./cometchat-regular-reaction-view/cometchat-regular-reaction-view.component";
+import { PickerModule } from "@ctrl/ngx-emoji-mart";
+import { EmojiModule } from "@ctrl/ngx-emoji-mart/ngx-emoji";
 
 @NgModule({
   declarations: [CometchatRegularReactionViewComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, PickerModule, EmojiModule],
+  exports: [CometchatRegularReactionViewComponent],
 })
-export class CometchatRegularReactionViewModule { }
+export class CometchatRegularReactionViewModule {}
