b81e2279533be33faa93a05c9503ab8a4f74f48f
Deepanshu Goyal <deepanshug@geekyants.com>
2021-01-04 13:34:45 +0530

    imported reply-count module

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/components/cometchat-receiver-file-bubble/cometchat-receiver-file-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/components/cometchat-receiver-file-bubble/cometchat-receiver-file-bubble.module.ts
@@ -4,10 +4,17 @@ import { CometchatReceiverFileBubbleComponent } from "./cometchat-receiver-file-
 import { CometchatReadRecieptModule } from "../cometchat-read-reciept/cometchat-read-reciept.module";
 import { CometchatToolTipModule } from "../cometchat-tool-tip/cometchat-tool-tip.module";
 import { CometchatAvatarModule } from "../cometchat-avatar/cometchat-avatar.module";
+import { CometchatReplyCountModule } from "../cometchat-reply-count/cometchat-reply-count.module";
 
 @NgModule({
   declarations: [CometchatReceiverFileBubbleComponent],
-  imports: [CommonModule, CometchatToolTipModule, CometchatAvatarModule],
+  imports: [
+    CommonModule,
+    CometchatToolTipModule,
+    CometchatAvatarModule,
+    CometchatReadRecieptModule,
+    CometchatReplyCountModule,
+  ],
   exports: [CometchatReceiverFileBubbleComponent],
 })
 export class CometchatReceiverFileBubbleModule {}
