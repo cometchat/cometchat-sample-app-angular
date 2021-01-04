06eb603ef0ddb14695d04bb2152d534b072ca545
Deepanshu Goyal <deepanshug@geekyants.com>
2021-01-04 13:45:32 +0530

    imported replycount module

--- CometChatWorkspace/projects/angular-chat-ui-kit/src/components/cometchat-sender-audio-bubble/cometchat-sender-audio-bubble.module.ts
+++ CometChatWorkspace/projects/angular-chat-ui-kit/src/components/cometchat-sender-audio-bubble/cometchat-sender-audio-bubble.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from "@angular/common";
 import { CometchatSenderAudioBubbleComponent } from "./cometchat-sender-audio-bubble/cometchat-sender-audio-bubble.component";
 import { CometchatToolTipModule } from "../cometchat-tool-tip/cometchat-tool-tip.module";
 import { CometchatReadRecieptModule } from "../cometchat-read-reciept/cometchat-read-reciept.module";
+import { CometchatReplyCountModule } from "../cometchat-reply-count/cometchat-reply-count.module";
 
 @NgModule({
   declarations: [CometchatSenderAudioBubbleComponent],
-  imports: [CommonModule, CometchatToolTipModule, CometchatReadRecieptModule],
+  imports: [
+    CommonModule,
+    CometchatToolTipModule,
+    CometchatReadRecieptModule,
+    CometchatReplyCountModule,
+  ],
   exports: [CometchatSenderAudioBubbleComponent],
 })
 export class CometchatSenderAudioBubbleModule {}
