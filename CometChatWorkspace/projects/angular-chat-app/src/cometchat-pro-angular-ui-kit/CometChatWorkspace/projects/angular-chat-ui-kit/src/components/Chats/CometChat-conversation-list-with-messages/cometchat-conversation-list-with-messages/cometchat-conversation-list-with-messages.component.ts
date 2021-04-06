d8f05052a5e153ec33a552875c53a0cbc4b07b4d
ajay.cometchat <ajay.gajra@inscripts.in>
2021-04-06 19:18:18 +0530

    Support for CometChat SDK v3.0.0-beta1 added

--- CometChatWorkspace/projects/angular-chat-app/src/cometchat-pro-angular-ui-kit/CometChatWorkspace/projects/angular-chat-ui-kit/src/components/Chats/CometChat-conversation-list-with-messages/cometchat-conversation-list-with-messages/cometchat-conversation-list-with-messages.component.ts
+++ CometChatWorkspace/projects/angular-chat-app/src/cometchat-pro-angular-ui-kit/CometChatWorkspace/projects/angular-chat-ui-kit/src/components/Chats/CometChat-conversation-list-with-messages/cometchat-conversation-list-with-messages/cometchat-conversation-list-with-messages.component.ts
@@ -702,7 +702,7 @@ export class CometChatConversationListWithMessagesComponent implements OnInit {
 
       //marking the incoming call message as read
       if (incomingCallMessage.hasOwnProperty(enums.READ_AT) === false) {
-        CometChat.markAsRead(incomingCallMessage.id, receiverId, receiverType);
+        CometChat.markAsRead(incomingCallMessage);
       }
 
       //updating unreadcount in chats list
