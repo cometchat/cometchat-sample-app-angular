02488a9df9b9f41f34a045068796731c4a5061c8
ananyasisodia <ananya.singh@cometchat.com>
2021-11-18 11:44:16 +0530

    internal project folder deleted

--- CometChatWorkspace/src/cometchat-pro-angular-ui-kit/CometChatWorkspace/projects/angular-chat-ui-kit/src/components/angular-chat-ui-kit.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { AngularChatUiKitComponent } from "./angular-chat-ui-kit.component";
-
-@NgModule({
-  declarations: [AngularChatUiKitComponent],
-  imports: [],
-  exports: [AngularChatUiKitComponent],
-})
-export class AngularChatUiKitModule {}
