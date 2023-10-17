7831dd73fd56f39d4ae9889fabaf8f78bcab2726
rajdubey <raj.dubey@cometchat.com>
2023-10-17 13:48:38 +0530

    changes

--- src/CONSTS.ts
+++ src/CONSTS.ts
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-   APP_ID: "208136f5ba34a5a2",
-   REGION: "us",
-   AUTH_KEY: "0cbf4d625ef2b877732039f9976c128459878ad4",
+  APP_ID: "APP_ID",
+  REGION: "REGION",
+  AUTH_KEY: "AUTH_KEY",
 };
