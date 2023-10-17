da1e2ac205f0abe973e4c23378be2129b30333e7
rajdubey <raj.dubey@cometchat.com>
2023-10-17 13:46:48 +0530

    changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cometchat-angular-sample-app'),
+      dir: require('path').join(__dirname, './coverage/cometchat-chat-sample-app-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
